81e0d32cf63ab3128ff8f4945177da188765c303
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-22 13:42:04 -0500

    CS61B Lecture 1 Intro

--- Head_First_Java/chapter_01/HeadFirstJava.java
+++ Head_First_Java/chapter_01/HeadFirstJava.java
@@ -1,4 +1,11 @@
 package chapter_01;
 
 public class HeadFirstJava {
+
+    public static void main(String[] args) {
+        int x = 5;
+        int y = x +3;
+        System.out.println(y);
+    }
+
 }
