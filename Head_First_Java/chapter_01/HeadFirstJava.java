6693840f5ab0fb04899984a8e424de94e0814af3
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-22 16:13:03 -0500

    CS61B Lecture 1 Intro

--- Head_First_Java/chapter_01/HeadFirstJava.java
+++ Head_First_Java/chapter_01/HeadFirstJava.java
@@ -4,7 +4,7 @@ public class HeadFirstJava {
 
     public static void main(String[] args) {
         int x = 5;
-        int y = x +3;
+        int y = x +3;  // must also claim y's type
         System.out.println(y);
     }
 
