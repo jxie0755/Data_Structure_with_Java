be9c023133a4399165d07a14214bc41f5e43f422
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-23 13:11:25 -0500

    Learn print

---
+++ Head_First_Java/chapter_01/C02_Print.java
@@ -0,0 +1,25 @@
+package chapter_01;
+
+public class C02_Print {
+    public static void main(String[] args) {
+        System.out.print("1");
+        System.out.print("2");
+        System.out.print("3");
+        System.out.println("4");  // println suggest next line will be new line, not this one
+        System.out.print("new line");
+        System.out.print("5");
+        System.out.print("6");
+        System.out.print("7");
+        // >>>
+        //1234
+        //new line567
+
+
+
+    }
+}
+
+/*
+ * print command (keep next print/println command at the same as this line)
+ * println command (keep next print/println command at a new line)
+ */
