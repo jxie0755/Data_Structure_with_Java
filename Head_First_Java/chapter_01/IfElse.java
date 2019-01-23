8c4a6714dde07bc49ce6b51e0f3240508309080e
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-23 13:04:37 -0500

    Head first java learn if elif else conditions

---
+++ Head_First_Java/chapter_01/IfElse.java
@@ -0,0 +1,39 @@
+package chapter_01;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        System.out.println("if then no else: ");
+        if (3 < 5) {
+            System.out.println("3 < 5!!");
+        }
+        System.out.println("This should show as it is outside of if loop");
+
+
+        System.out.println("");
+        System.out.println("Simple if else: ");
+        if (3 < 5) {
+            System.out.println("3 < 5!!");
+        } else {
+            System.out.println("This should not show");
+        }
+
+
+        System.out.println("");
+        System.out.println("if elif else: ");
+        if (3 < 5) {
+            System.out.println("3 < 5!!");
+        } else if (2 < 5){
+            System.out.println("elif 2 < 5");
+        } else {
+            System.out.println("Not showen");
+        }
+
+    }
+}
+
+
+/*
+* Same if/else, if/elif/else logic as python
+* else if will be skipped if first "if" was true
+* */
