478b977981f7717ae1a7ebc0b756356074c30934
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-23 13:16:43 -0500

    Head First Java Learn while loop

---
+++ Head_First_Java/chapter_01/C04_While_Loop.java
@@ -0,0 +1,33 @@
+package chapter_01;
+
+public class C04_While_Loop {
+    public static void main(String[] args) {
+
+        System.out.println("while loop");
+
+        int x = 0;
+        while (x <= 10) {
+            System.out.println(x);
+            x = x + 1;
+            // >>>
+            // while loop
+            //0
+            //1
+            //2
+            //3
+            //4
+            //5
+            //6
+            //7
+            //8
+            //9
+            //10
+        }
+    }
+}
+
+/*
+ * for loop defines start value, limit, and step
+ * while loop is more like python, define indicator outside
+ * java中,int和boolean不同, 所以不能while(0)或者while(1)
+ */
