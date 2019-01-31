11213461224cf38e2302d05f1aa6d063d1c94a46
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-31 15:45:34 -0500

    Java Reference Learn array

---
+++ Java_Reference/java_reference_chapter_01/C11_Arrays.java
@@ -0,0 +1,55 @@
+package java_reference_chapter_01;
+
+public class C11_Arrays {
+
+    // Practice of finding primes with The Sieve of Eratosthenes
+    /** Returns an array, p, of size N+1 such that p[k] is true iff
+      * k is a prime. */
+    public static boolean[] primes (int n) {
+        boolean[] sieve = new boolean[n + 1];  // a list of boolean length at n+1, initialized list will be all false!
+        // All entries are initially true.
+        sieve[0] = sieve[1] = true;
+        sieve[2] = false;
+        for (int k = 2; k * k <= n; k += 1) {
+            for (int j = k * k; j <= n; j += k) {
+                sieve[j] = true;
+            }
+        }
+        return sieve;
+    }
+
+    public static void main(String[] args) {
+        int[] A;
+        // System.out.println(A);  Not initialized, only created a null point (linked list)
+        A = new int[]{1, 2, 3};
+        System.out.println(A);  // >>> [I@1e643faf   // Does not print out A
+        System.out.println(A.length);  // >>> 3
+
+        A = new int[100];  // this create a new int list of length at 100
+        System.out.println(A.length);  // >>> 100
+
+        boolean[] S = primes(20);
+        int i = 0;
+        while (i != S.length) {
+            if (!S[i]) {
+                System.out.println(i);
+            }
+            i += 1;
+        }
+        // >>>
+        // 2
+        // 3
+        // 5
+        // 7
+        // 11
+        // 13
+        // 17
+        // 19
+
+
+    }
+
+
+
+}
+
