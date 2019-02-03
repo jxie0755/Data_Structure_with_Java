4bb3a4306ae7fd01cd294f0c008fd4f8bafa09ac
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-02-03 11:27:48 -0500

    W3Cschool Java Learn OOP static

--- Java_W3Cschool/OOP_Test_Pubic_Above.java
+++ Java_W3Cschool/OOP_Test_Pubic_Above.java
@@ -5,16 +5,16 @@ public class OOP_Test_Pubic_Above {
     public static void main(String[] args) {
 
         // Test public
-        C05_OOP_A AAA = new C05_OOP_A();
-        // 05_OOP_B BBB = new C05_OOP_B();  // 内部类不能被任何访问
-        // C05_OOP_C CCC = new C05_OOP_C();  // 不是public所以不能被import
+        C05_OOP_A AA = new C05_OOP_A();
+        // 05_OOP_B BB = new C05_OOP_B();  // 内部类不能被任何访问
+        // C05_OOP_C CC = new C05_OOP_C();  // 不是public所以不能被import
 
         // Test static // 只要是public都可以
-        C05_OOP_AA AAAA = new C05_OOP_AA();   // 真正原因是因为这里造了一个实例,非static的变量和方法必须被实例使用
-        System.out.println(AAAA.fooo);
-        System.out.println(AAAA.barrr);
-        AAAA.foo();
-        AAAA.fooo_barrr();
+        C05_OOP_AA AAA = new C05_OOP_AA();   // 真正原因是因为这里造了一个实例,非static的变量和方法必须被实例使用
+        System.out.println(AAA.fooo);
+        System.out.println(AAA.barrr);
+        AAA.foo();
+        AAA.fooo_barrr();
         // 直接从类出发:
         System.out.println(C05_OOP_AA.fooo);       // 由于fooo是static所以可以直接被call
         C05_OOP_AA.foo();                          // 同上
