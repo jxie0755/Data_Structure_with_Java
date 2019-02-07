dcc204c4d6266faf82eadbe0fcc41219146be3df
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-02-07 18:20:41 -0500

    Learn Java OOP Learn initiation

---
+++ Java_W3Cschool/W3C_Java_Advaned_OOP/C05_Initiation.java
@@ -0,0 +1,64 @@
+package W3C_Java_Advaned_OOP;
+
+public class C05_Initiation {
+}
+
+
+// 子类是否必须要重写父类的构造函数?
+
+// 若父类只有构造函数, 则子类必须重写
+class Superman_1 {
+
+    String name;
+    public Superman_1 (String hero_name) {
+        this.name = hero_name;
+    }
+}
+
+class Supermario_1 extends Superman_1 {
+    public Supermario_1 (String hero_name) {
+        super(hero_name);
+    }
+}
+
+
+
+// 若父类只有构造函数, 同时还重载了一个无参构造函数, 则子类不必重写
+class Superman_2 {
+
+    String name;
+    public Superman_2(String hero_name) {
+        this.name = hero_name;
+    }
+
+    public Superman_2 () {
+
+    }
+}
+
+class Supermario_2 extends Superman_2 { }
+
+
+// 如果父类构造函数不公开呢?
+class Superman_3 {
+
+    private String name;
+    private Superman_3(String hero_name) {
+        this.name = hero_name;
+    }
+
+    public Superman_3 () {
+
+    }
+
+}
+
+class Supermario_3 extends Superman_3 {
+
+    // 必须要有一个public的构造器提供重载才能被重写
+    String name;
+    public Supermario_3(String mario_name) {
+        this.name = mario_name;
+    }
+
+}
