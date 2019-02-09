47c04fbf3e4b99e38c10f1f1b9bdb3e6242cf6ba
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-02-09 09:56:50 -0500

    Learn Java OOP Learn Casting

--- Java_W3Cschool/W3C_Java_Advaned_OOP/C10_Instance_Casting.java
+++ Java_W3Cschool/W3C_Java_Advaned_OOP/C10_Instance_Casting.java
@@ -103,11 +103,13 @@ class Material {
 //盐类
 class Salt extends Material {
     public void intro() { System.out.println("我是盐"); }
+    public void health() { System.out.println("做菜少放盐!");}
 }
 
 //肉类
 class Meat extends Material {
     public void intro() { System.out.println("我是肉"); }
+    public void price() { System.out.println("我很贵!!");}
 }
 
 //蔬菜类
@@ -118,13 +120,28 @@ class Vegetable extends Material {
 
 class MyMenu {
 
+    // 实战:
     // 不用casting,为三个子类分别写方法, 相当于使用重载overload
-    public static void add(Salt m) { m.intro(); }
-    public static void add(Meat m) { m.intro(); }
+    public static void add(Salt m) { m.intro(); m.health(); }
+    public static void add(Meat m) { m.intro(); m.price(); }
     public static void add(Vegetable m) { m.intro(); }
 
+
     // 使用casting
-    public static void add_2(Material m) { m.intro(); }
+    // 向上向下转型混合实战:
+    public static void add_2(Material m) {  // 向上转型
+        if (m instanceof Salt) {
+            Salt m_2 = (Salt) m;     // 需要对Salt特殊health(), 所以向下转型, 使用if来安全执行
+            m_2.intro();
+            m_2.health();
+        } else if (m instanceof  Meat) {
+            Meat m_2 = (Meat) m;    // 需要对Meat特殊health(), 所以向下转型, 使用else if来安全执行
+            m_2.intro();
+            m_2.price();
+        } else {
+            m.intro();      // 不做特殊处理的话就是单纯向上转型即可
+        }
+    }
 
     public static void main(String[] args) {
         add(new Salt());
@@ -134,118 +151,31 @@ class MyMenu {
         // add_2因为casting可以同时套用到三个子类上,分别输出子类的方法, 这样节省了重复代码
         // 向上转型是统一的单个方法来执行子类中重写的不同方法, 避免重载的麻烦
         add_2(new Salt());
+        // >>>
+        // 我是盐
+        // 做菜少放盐
+
         add_2(new Meat());
+        // >>>
+        // 我是肉
+        // 我很贵!!
+
         add_2(new Vegetable());
+        // >>>
+        // 我是蔬菜
         // 如果我又有一种新菜加进来，我只需要实现它自己的类，让他继承Material就可以了，而不需要为它单独写一个add方法。是不是提高了扩展性?
     }
 }
 
-
-
-
-// 向下转型详细解释:
-class Animal_7 {
-    public void eat(){ System.out.println("animal eatting..."); }
-}
-
-class Cat_7 extends Animal_7{
-    public void eat(){ System.out.println("我吃鱼"); }
-}
-
-class Dog_7 extends Animal_7{
-    public void eat(){ System.out.println("我吃骨头"); }
-    public void run(){ System.out.println("我会跑"); }
-}
-
-class Animal_7_Test {
-
-    public static void main(String[] args) {
-        Animal_7 a = new Cat_7();
-        Cat_7 c = (Cat_7) a;  // c也就是a转成Cat的cast
-        System.out.println(a instanceof Cat_7); // >>> true
-        c.eat();  // >>> 我吃鱼
-        // 为什么第一段代码不报错呢？因为a本身就是Cat对象，所以它理所当然的可以向下转型为Cat，但是这是无效操作
-
-        // Dog_7 d = (Dog_7) a;  // 尝试吧a转型成Dog, 不能成功
-        // System.out.println(d instanceof  Dog_7);
-        // d.eat();  报错
-
-        Animal_7 a1 = new Animal_7();
-        System.out.println(a1 instanceof Cat_7); // >>> false
-        // Cat_7 c1 = (Cat_7) a1;  // 尝试吧a1转型成Cat类,a1本身是动物类
-        // c1.eat(); 报错
-
-        // 正确操作
-        Animal_7 a_x = new Cat_7();  // Cat_7新造的实例被cast成Animal类, 所以是先向上转型
-        System.out.println(a_x.getClass()); // >>> Cat_7
-
-        Animal_7 a_x_cast = (Animal_7) a_x;  // 向下转型
-        System.out.println(a_x_cast.getClass()); // >>> Cat_7
-        a_x_cast.eat();    // >>> 我吃鱼
-        // 向下转型的前提是父类对象指向的是子类对象（也就是说，在向下转型之前，它得先向上转型）
-        // 向下转型只能转型为子类对象（猫是不能变成狗的,因为他们是平行关系）
-
-
-        // 这样就用一个eat来执行Dog和Cat各自独特的方法
-        eat(new Dog_7());
-        // >>>
-        // 我吃骨头
-        // 我会跑
-
-        eat(new Cat_7());
-        // >>>
-        // 我吃鱼
-        // 我也想跑，但是不会
-
-    }
-
-    // 用法:  向下转型就是根据子类的不同,执行子类独有的,父类没有的方法
-    public static void eat (Animal_7 a){
-            if (a instanceof Dog_7) {
-                Dog_7 d = (Dog_7) a;
-                d.eat();
-                d.run(); // 狗有一个跑的方法 (这里如果单用向上转型,则不行,因为不是每个Animal都有run这个方法,必须是向下转型,使得父类能够在if成立时调用子类方法
-            } else if (a instanceof Cat_7) {
-                Cat_7 c = (Cat_7) a;
-                c.eat();
-                System.out.println("我也想跑，但是不会"); // 猫会抱怨, 也算是一个附加处理,比其他类只执行eat()不同
-            } else { a.eat();//其他动物只会吃
-            }
-    }
-
-}
 
 /*
  * 用法总结:
     * 向上转型和向下转型,原则上都是用父类作为一个方法的参数(向上转型),让它分别执行子类的方法
         * 向上转型的用处是, 执行各子类相同的方法时,避免重载导致代码重复
         * 向下转型的用处是, 能够使父类参数执行子类中独特的方法(附加处理)或者是父类没有的方法
+    * 这么做的动机:
+        * 方法中只需要用定义一个父类的实例参数,让他们能在方法中展现出子类的特性, 由于类型检查的特性, 这个父类实例需要被看成是子类实例才能去继承子类的方法
+        * 有些子类中的方法是父类也有的, 虽然被重写, 这样直接对子类call这个方法时安全的(向上转型)
+        * 有些子类的方法是父类没有的,或者需要对子类进行特别操作, 因此需要根据子类类型来分别对待,这时这个父类参数必须是安全的向下转型
     * 这些都是python作为动态语言不会出现的问题,因为python不检查参数类型,而java会检查,导致了参数不能多态
  */
-
-
-class AAAA {
-
-    void foo() {
-        System.out.println("From AAAA");
-    }
-
-}
-
-class BBBB extends  AAAA {
-
-    void foo() {
-        System.out.println("From BBBB");
-    }
-
-}
-
-class Test_AAAA {
-
-    public static void main(String[] args) {
-        AAAA aaaa = new BBBB();
-        ((BBBB) aaaa).foo();
-
-    }
-
-}
