116439996cbc9aa2d249ff43a8b2a23a58ba1ecf
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-02-14 15:25:30 -0500

    Learn Number and Math

--- Java_W3Cschool/W3C_Java_Classes/C02_Math.java
+++ Java_W3Cschool/W3C_Java_Classes/C02_Math.java
@@ -5,16 +5,41 @@ public class C02_Math {
 
 class Math_Method_Test {
     public static void main(String[] args) {
+        // ceil, floor, and rint
+        // 返回doubnle值
+        Double d1 = 1.499999;
+        Double d2 = Math.ceil(d1);   // 2.0
+        Double d3 = Math.floor(d1);  // 1.0
+        Double d4 = Math.rint(d1);   // 分界于0.499和0.500
+        System.out.println(d2 + " " + d3 + " " + d4);
+
+
+        // round
+        // 返回int, long值
+        int i1 = Math.round(1.9f);   // 2  // float参数返回int或long
+        long l1 = Math.round(1.9d);  // 2  // double参数返回long
+        System.out.println(i1 + " " + l1);
+
         // abs
         // 取绝对值(double, float, int, long)
-        Integer x1 = -128;
-        Integer x1abs = Math.abs(x1);  // 128
+        Integer i2 = -128;
+        Integer i2abs = Math.abs(i2);  // 128
 
         // exp
         double e = Math.E;  // 2.718281828459045
-        Integer x2 = 3;
-        Double x2exp = Math.exp(x2);  // e^3
+        Integer i3 = 3;
+        Double i3exp = Math.exp(i3);  // e^3
 
+        // log, log10
+        double d5 = Math.log(Math.exp(i3));  // 3.0
+        double d6 = Math.log10(100);         // 2.0
+
+        // pow
+        double d7 = Math.pow(2, 3);  // 8.0
+
+        // sqrt and cbrt
+        double d8 = Math.sqrt(16);  // 4.0
+        double d9 = Math.cbrt(27);  // 3.0
 
         // 三角函数
         System.out.println("90 度的正弦值：" + Math.sin(Math.PI/2));
@@ -23,6 +48,24 @@ class Math_Method_Test {
         System.out.println("1的反正切值： " + Math.atan(1));
         System.out.println("π/2的角度值：" + Math.toDegrees(Math.PI/2));
         System.out.println(Math.PI);
+
+
+        // Max and Min
+        // int long double float
+        double d10 = Math.max(10, 9);  // 10.0
+        double d11 = Math.min(10, 9);  // 9.0
+
+
+        // random
+        // Returns a double value with a positive sign,
+        // greater than or equal to 0.0 and less than 1.0
+        double x = Math.random();
+        System.out.println(x);
+        // As the largest double value less than 1.0 is Math.nextDown(1.0),
+        // a value x in the closed range [x1,x2] where x1<=x2
+        // may be defined by the statements
+        double f = Math.random()/Math.nextDown(1.0);
+        double x_range = 3*(1.0 - f) + 5*f;  // range(3, 5)
     }
 }
 
