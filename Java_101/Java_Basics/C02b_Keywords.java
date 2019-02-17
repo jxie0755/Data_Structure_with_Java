6601efda66bde78007fb575d109ee4d19c832ad3
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-02-17 15:31:03 -0500

    Learn java Keywords

---
+++ Java_101/Java_Basics/C02b_Keywords.java
@@ -0,0 +1,73 @@
+package Java_Basics;
+
+public class C02b_Keywords {
+}
+
+// java一共有50个关键字
+
+/*
+ * Premitive types
+     * byte
+     * boolean
+     * char
+     * double
+     * float
+     * int
+     * long
+     * short
+ * Return value
+     * void
+     * return
+ * Project structure
+     * import
+     * package
+ * Class publicity
+     * default
+     * public
+     * protected
+     * private
+ * Class inheritance relationships
+     * impletments
+     * extends
+ * Class types
+     * abstract
+     * class
+     * interface
+ * Class methods
+     * super
+     * instanceof
+     * new
+     * this
+ * Class variables
+     * static
+     * final
+ * Assertions
+     * assert
+ * Exceptions
+     * try
+     * catch
+     * throw
+     * throws
+     * finally
+ * Logic flow control
+     * if
+     * else
+     * for
+     * while
+     * do
+     * switch
+     * case
+     * break
+     * continue
+ * Others
+     * enum
+     * native
+     * strictfp
+     * synchronized
+     * transient
+     * volatile
+ * Reserved Keywords
+     * const
+     * goto
+ */
+
