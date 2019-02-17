0af1bfdb663b848fb3d13c51b03c11dc061312fb
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-02-17 15:40:25 -0500

    Learn java Keywords

--- Java_101/Java_Basics/C02b_Keywords.java
+++ Java_101/Java_Basics/C02b_Keywords.java
@@ -33,6 +33,7 @@ public class C02b_Keywords {
      * abstract
      * class
      * interface
+     * enum      // 枚举类
  * Class methods
      * super
      * instanceof
@@ -60,12 +61,11 @@ public class C02b_Keywords {
      * break
      * continue
  * Others
-     * enum
-     * native
-     * strictfp
-     * synchronized
-     * transient
-     * volatile
+     * native         // native是与C++联合开发的时候用的！java自己开发不用的！
+     * strictfp       // strict float point (精确浮点), 保证浮点运算精确, 不受不同平台干扰
+     * synchronized   // 多线程编程时使用, 当它用来修饰一个方法或者一个代码块的时候，能够保证在同一时刻最多只有一个线程执行该段代码
+     * volatile       // 多线程编程时使用, volatile关键字用来修饰变量，作用是使得该变量在多个线程之间可见
+     * transient      // 在序列化和反序列化的时候，可以进行关键字的屏蔽，只对需要进行持久化的字段进行序列化
  * Reserved Keywords
      * const
      * goto
