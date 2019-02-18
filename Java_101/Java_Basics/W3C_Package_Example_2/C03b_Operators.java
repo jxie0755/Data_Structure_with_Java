d92127302059537147e4703b1d9adebf3a90682b
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-02-18 16:32:57 -0500

    Learn operators

--- Java_101/Java_Basics/W3C_Package_Example_2/C03b_Operators.java
+++ Java_101/Java_Basics/W3C_Package_Example_2/C03b_Operators.java
@@ -2,6 +2,7 @@ package Java_Basics.W3C_Package_Example_2;
 
 public class C03b_Operators {
     public static void main(String[] args) {
+
         // 一元运算符, 意思是只要一个参数
         int a = 12;
         System.out.println(-a);  // >>> -12
@@ -17,6 +18,7 @@ public class C03b_Operators {
         b2 = --a2;
         System.out.println(b2);  // >>> 10 (a = 11, a -= 1, b = a)
 
+
         // 二元运算符, 普通+-*/%之类,需要两个参数的, 这些运算符对数值类型数据都有效 (也就是除了boolean以外的基本类型)
 
         // int和char交互 (参见ascii table)
@@ -25,6 +27,7 @@ public class C03b_Operators {
         int int_char = ch1 + ch2;  // 68
         char ch3 = (char) int_char;
         System.out.println(ch3);  // D
+
 
         // 关系运算符
         // ==, !=, >, <, >=, <=
@@ -35,6 +38,37 @@ public class C03b_Operators {
         String s2 = new String("Hello");
         System.out.println(s1 == s2);       // >>>  false
         System.out.println(s1.equals(s2));  // >>>  true
+
+
+        // 逻辑运算符
+        // !, &, |
+        // 短路: &&, ||   // 优先用短路, 不再赘述原理
+
+
+        // 位运算 (不赘述, 需要时再看, 参见python bit calculation)
+        /*
+         * &
+         * |
+         * ^
+         * ~
+         * >>
+         * <<
+         * >>>
+         */
+
+
+        // 其他运算符
+        /*
+         * 三元运算符（? :） - 例如x?y:z;， 其中x、 y和z都为表达式。
+         * 小括号          -  起到改变表达式运算顺序的作用， 它的优先级最高。
+         * 中括号          - 数组下标。
+         * 引用号（.）      - 对象调用实例变量或实例方法的操作符， 也是类调用静态变量或静态方法的操作符。
+         * 赋值号（=）      - 赋值是用等号运算符（=） 进行的。
+         * instanceof     - 判断某个对象是否为属于某个类。
+         * new            - 对象内存分配运算符。
+         * 箭头（->）       - Java 8新增加的， 用来声明Lambda表达式。
+         * 双冒号（::）     - Java 8新增加的， 用于Lambda表达式中方法的引用。
+         */
     }
 }
 
