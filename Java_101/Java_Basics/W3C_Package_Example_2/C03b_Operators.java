8b2b5a0b778b3511fe1c6bc67bf33f2b94519cef
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-02-18 15:53:27 -0500

    Learn operators

--- Java_101/Java_Basics/W3C_Package_Example_2/C03b_Operators.java
+++ Java_101/Java_Basics/W3C_Package_Example_2/C03b_Operators.java
@@ -2,7 +2,7 @@ package Java_Basics.W3C_Package_Example_2;
 
 public class C03b_Operators {
     public static void main(String[] args) {
-        // 一元运算符
+        // 一元运算符, 意思是只要一个参数
         int a = 12;
         System.out.println(-a);  // >>> -12
 
@@ -17,8 +17,24 @@ public class C03b_Operators {
         b2 = --a2;
         System.out.println(b2);  // >>> 10 (a = 11, a -= 1, b = a)
 
-        //
+        // 二元运算符, 普通+-*/%之类,需要两个参数的, 这些运算符对数值类型数据都有效 (也就是除了boolean以外的基本类型)
 
+        // int和char交互 (参见ascii table)
+        char ch1 = '!';   // 33
+        char ch2 = '#';   // 35
+        int int_char = ch1 + ch2;  // 68
+        char ch3 = (char) int_char;
+        System.out.println(ch3);  // D
+
+        // 关系运算符
+        // ==, !=, >, <, >=, <=
+        // 提示 ==和!=可以应用于基本数据类型和引用数据类型。
+        // 当用于引用数据类型比较时， 比较的是两个引用是否指向同一个对象， 但在实际开发过程中多数情况下， 只是比较对象的内容是否相当， 不需要比较是否为同一个对象
+        // 这样的话, 需要用equals这种方法来做比较,不要直接用==, 否则两个相同的量的引用类, 用==出来是不同的!
+        String s1 = new String("Hello");
+        String s2 = new String("Hello");
+        System.out.println(s1 == s2);       // >>>  false
+        System.out.println(s1.equals(s2));  // >>>  true
     }
 }
 
