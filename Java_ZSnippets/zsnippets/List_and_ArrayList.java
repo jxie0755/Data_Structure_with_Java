b98154a537cdb24ecc13532c63dd576816885ab5
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-02-24 15:21:33 -0500

    Learn difference of List and ArrayList

--- Java_ZSnippets/zsnippets/List_and_ArrayList.java
+++ Java_ZSnippets/zsnippets/List_and_ArrayList.java
@@ -22,7 +22,9 @@ public class List_and_ArrayList {
 
             * List list<Object> = new ArrayList();
                 * 这句创建了一个ArrayList的对象后把上溯到了List
-                * 此时它是一个List对象了，有些ArrayList有但是List没有的属性和方法，它就不能再用了
+                * 此时它是一个List对象了
+                * 它只支持List的方法, (但是有些List的方法ArrayList也有,这部分它是支持的, (多态))
+                * 但是也有一部分ArrayList独有的方法, 它就不会支持了
 
             * ArrayList<Object> list=new ArrayList();
                 * 创建一对象则保留了ArrayList的所有属性
@@ -35,10 +37,99 @@ public class List_and_ArrayList {
         /*
          * 如果 List a=new ArrayList();
             * 则a拥有List与ArrayList的所有属性和方法，不会减少
-            * 如果List与ArrayList中有相同的属性（如int i), 则a.i是调用了List中的i
+            * 如果List与ArrayList中有相同的属性（如int i), 则a.i是调用了ArrayList中的i
             * 如果List与ArrayList中有相同的方法（如void f()),a.f()是调用了ArrayList中的f()
+            * 但是a没有ArrayList独有的,而List没有的方法
          */
 
+
+        /*
+         * 为什么要用 List list = new ArrayList() ,而不用 ArrayList alist = new ArrayList()呢？
+         * 问题就在于List有多个实现类，现在你用的是ArrayList，也许哪一天你需要换成其它的实现类，
+            * 如 LinkedList或者Vector等等，这时你只要改变这一行就行了:
+            * List list = new LinkedList(); 其它使用了list地方的代码根本不需要改动
+                * 理解: List作为父类, 它可以接受各种其他子类和它共有的方法, 这个优势如果建立ArrayList就没有.
+            * 假设你开始用 ArrayList alist = new ArrayList(), 这下你有的改了，特别是如果你使用了 ArrayList特有的方法和属性
+
+            * 所以这个事是各有利弊:
+                * 用List = 就更通用,但是不能使用子类的独特特性
+                * 用ArrayList = 就专注于ArrayList的独特特性, 但是不好换型
+         */
     }
 }
 
+// 以下是更清晰的继承与多态的方法和属性演示:
+class Father {
+    int i;
+    public Father (){
+        this.i = 0;
+    }
+
+    public void foo(int n) {
+        System.out.print("from Father" );
+        System.out.println(n*2);
+    }
+}
+
+class Child1 extends Father {
+    int i;
+    public Child1 (){
+        this.i = 1;
+    }
+
+    public void foo(int n) {
+        System.out.print("from Child 11111111 ");
+        System.out.println(n*5);
+    }
+
+    public void bar(int n) {
+        System.out.print("from Child 11111111 ");
+        System.out.println(n*500);
+    }
+}
+
+class Child2 extends Father {
+    int i;
+    public Child2 (){
+        this.i = 2;
+    }
+
+    public void foo(int n) {
+        System.out.print("from Child 222222 ");
+        System.out.println(n*513);
+    }
+
+    public void bar(int n) {
+        System.out.print("from Child 2222 ");
+        System.out.println(n*11100);
+    }
+}
+
+class Tttt {
+
+    public static void main(String[] args) {
+        Child1 C1 = new Child1();
+        Child2 C2 = new Child2();
+
+        Father F1 = new Child1();
+        Father F2 = new Child2();
+
+        // 各自继承自己的属性, 没有问题
+        System.out.println(C1.i); // >>> 1
+        System.out.println(C2.i); // >>> 2
+        System.out.println(F1.i); // >>> 0
+        System.out.println(F2.i); // >>> 0
+
+        C1.foo(5); // >>> from Child 11111111 25
+        C1.bar(5); // >>> from Child 11111111 2500
+
+        C2.foo(5); // >>> from Child 222222 2565
+        C2.bar(5); // >>> from Child 2222 55500
+
+        F1.foo(5); // >>>  from Child 11111111 25  // 顺利的继承了Child1的方法
+        // F1.bar(5); // F1没有bar, 因为Father 没有Bar(), 虽然new Child1()有
+
+        F2.foo(5); // >>>  from Child 222222 2565  // 顺利的继承了Child1的方法
+        // F2.bar(5); // F2也没有bar, 因为Father 没有Bar(), 虽然new Child2()有
+    }
+}
