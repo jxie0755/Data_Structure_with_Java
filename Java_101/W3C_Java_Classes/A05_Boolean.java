9cd73b85ff7000ad6826481a690acefc503ea25f
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-03-01 15:41:06 -0500

    Learn Boolean

---
+++ Java_101/W3C_Java_Classes/A05_Boolean.java
@@ -0,0 +1,16 @@
+package W3C_Java_Classes;
+
+public class A05_Boolean {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Boolean_zMethods {
+
+    public static void main(String[] args) {
+
+    }
+}
+
