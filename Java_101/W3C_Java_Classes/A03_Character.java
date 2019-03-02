a607061844f32287e2f69a9c31563dc9211069c5
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-03-01 21:08:07 -0500

    Learn Character

--- Java_101/W3C_Java_Classes/A03_Character.java
+++ Java_101/W3C_Java_Classes/A03_Character.java
@@ -26,5 +26,14 @@ class Character_zMethods {
         // 字母AZ的数字值均为10至35.这与Unicode规范无关，其中不会为这些char值分配数值
         System.out.println(Character.getNumericValue(char1)); // >>> 12  (这个12是因为C是ascii中的第12个字符)
 
+        // Other methods
+        // isLetter()       // 是否是一个字母
+        // isDigit()        // 是否是一个数字字符
+        // isWhitespace()   // 是否一个空格
+        // isUpperCase()    // 是否是大写字母
+        // isLowerCase()    // 是否是小写字母
+        // toUpperCase()    // 指定字母的大写形式
+        // toLowerCase()    // 指定字母的小写形式
+        // toString()       // 返回字符的字符串形式，字符串的长度仅为1
     }
 }
