ca4dce2fb9ec6d4420e6b8a93c6eaf632e1e6f64
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-03-04 15:24:15 -0500

    Learn StringBuffer and StringBuilder

--- Java_101/W3C_Java_Classes/A05_StringBuffer_StringBuilder.java
+++ Java_101/W3C_Java_Classes/A05_StringBuffer_StringBuilder.java
@@ -1,5 +1,27 @@
 package W3C_Java_Classes;
 
 public class A05_StringBuffer_StringBuilder {
+
 }
 
+
+/*
+ * String 字符串常量
+ * StringBuffer 字符串变量（线程安全）
+ * StringBuilder 字符串变量（非线程安全）
+ */
+
+class z_StringBuffer {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class z_StringBuilder {
+
+    public static void main(String[] args) {
+
+    }
+}
