fb78f45e22b24ed4d7e6236275101e9b1b142477
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-03-04 21:36:25 -0500

    Update A05_StringBuffer_StringBuilder.java

--- Java_101/W3C_Java_Classes/A05_StringBuffer_StringBuilder.java
+++ Java_101/W3C_Java_Classes/A05_StringBuffer_StringBuilder.java
@@ -9,11 +9,20 @@ public class A05_StringBuffer_StringBuilder {
  * String 字符串常量
  * StringBuffer 字符串变量（线程安全）
  * StringBuilder 字符串变量（非线程安全）
+    * StringBuffer 和 StringBuilder 类的对象能够被多次的修改，并且不产生新的未使用对象
  */
 
 class z_StringBuffer {
 
     public static void main(String[] args) {
+
+        StringBuffer sBuffer = new StringBuffer("Denis: ");
+        sBuffer.append("www");
+        sBuffer.append(".helloworld");
+        sBuffer.append(".com");
+        System.out.println(sBuffer); // >>>  Denis: www.helloworld.com
+
+
 
     }
 }
