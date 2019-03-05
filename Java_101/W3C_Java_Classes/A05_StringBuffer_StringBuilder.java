f72b7cddd0715d5bd0e7ec5bc2837def2d60f760
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-03-04 22:36:01 -0500

    Learn StringBuffer and StringBuilder

--- Java_101/W3C_Java_Classes/A05_StringBuffer_StringBuilder.java
+++ Java_101/W3C_Java_Classes/A05_StringBuffer_StringBuilder.java
@@ -90,11 +90,6 @@ class z_StringBuffer {
             * String toString()
          */
 
-
-
-
-
-
     }
 }
 
@@ -103,5 +98,77 @@ class z_StringBuilder {
 
     public static void main(String[] args) {
 
+        // 可以说StringBuilder和StringBuffer使用上是一模一样,所有这些方法都能通用
+
+        // 构造方法非常类似String
+        StringBuilder sBuilder = new StringBuilder("Cindy: ");
+        // StringBuffer sB2 = "JoJoJo";  这是不允许的, Str和SB二者不是继承关系
+
+
+        // 可变类型 (append方法)
+        sBuilder.append("first");
+        sBuilder.append("second");
+        sBuilder.append("last");
+
+
+        // 可以直接打印
+        System.out.println(sBuilder);  // >>> Cindy: firstsecondlast
+
+        // reverse 反转
+        sBuilder.reverse();
+        System.out.println(sBuilder);  // >>> tsaldnocestsrif :ydniC
+        sBuilder.reverse();
+
+
+        // delete(int start, int end)
+        sBuilder.delete(1,5);
+        System.out.println(sBuilder);  // >>>  C indy : firstsecondlast
+                                        //     0 1234 5
+
+        // insert(int offset, int i)  将 int 参数的字符串表示形式插入此序列中
+        sBuilder.insert(1, 9);
+        System.out.println(sBuilder);  // >>>  C9: firstsecondlast
+
+
+        // replace(int start, int end, String str) 使用给定 String 中的字符替换此序列的子字符串中的字符
+        // C9: firstsecondlast
+        // 0123456789
+        sBuilder.replace(4, 8, "123456789");
+        System.out.println(sBuilder);
+        // >>> C9: 123456789tsecondlast
+
+
+        // void setCharAt(int index, char ch)
+        sBuilder.setCharAt(1, 'T');
+        System.out.println(sBuilder);
+        // >>>  CT: 123456789tsecondlast
+
+
+        // void setLength(int newLength)  保留长度内的字符串,多余的删去
+        sBuilder.setLength(3);
+        System.out.println(sBuilder);
+        // >>> CT:
+
+
+
+        // 其他方法
+        // capacity (与ArrayList的容量类似, 一般不必理会, 会自动扩容, 空白构造默认16长度的capacity)
+        System.out.println(sBuilder.capacity()); // >>>  48
+        // void ensureCapacity(int minimumCapacity)  确保容量至少等于指定的最小值
+
+        /*
+         * 其他和String类似的方法
+            * char charAt(int index)
+            * getChars(int srcBegin, int srcEnd, char[] dst, int dstBegin)
+            * int indexOf(String str)
+            * int indexOf(String str, int fromIndex)
+            * int lastIndexOf(String str)
+            * int lastIndexOf(String str, int fromIndex)
+            * int length()
+            * CharSequence subSequence(int start, int end)
+            * String substring(int start)
+            * String substring(int start, int end)  // 相当于slice切片
+            * String toString()
+         */
     }
 }
