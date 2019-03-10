b42ec418724d7702ad976137be3ced72e6b76f5c
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-03-09 20:44:23 -0500

    Re-organize

--- Java_101/Java_OOP/C07_Interface.java
+++ Java_101/Java_OOP/C07_Interface.java
@@ -154,3 +154,63 @@ class PremierLeauge_Game_1 implements Football, Events {
 // 空接口 (标记接口)
 interface EventListener_X {}
 
+
+
+
+/*
+ * Aditional
+ * 多重继承接口时出现同名方法(彻底重名,无法重载)
+ * 如果类型实现两个接口，并且每个接口定义具有相同签名的方法，则实际上只有一个方法，并且它们是不可区分的
+ */
+
+interface Friend_X {
+    public void play() ;
+    public void beFriendly();
+}
+
+interface Pet_X {
+    public void play() ;
+    public void beFriendly();
+}
+
+// 这样建立一个狗类,既是朋友,也是宠物!
+class Dog_X implements Friend_X, Pet_X {
+    @Override
+    public void play() {
+        System.out.println();
+    }
+    @Override
+    public void beFriendly(){}
+}
+
+// 还有一种方法就是使用内部类
+class Dog_Y implements Friend_X {
+
+    public void play() {
+        System.out.println("Play as Friend");
+    }
+    public void beFriendly() {
+        System.out.println("Friendly Friend");
+    }
+    public static class Dog_Y2 implements Pet_X { // 注意内部类要static
+        public void play() {
+        System.out.println("Play as Pet");
+        }
+        public void beFriendly() {
+            System.out.println("Friendly Pet");
+        }
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+
+        Dog_Y DX = new Dog_Y();
+        DX.beFriendly();  // >>> Friendly Friend
+        DX.play();        // >>> Play as Friend
+
+        Dog_Y.Dog_Y2 DX2 = new Dog_Y.Dog_Y2();
+        DX2.beFriendly();  // >>> Friendly Pet
+        DX2.play();        // >>> Play as Pet
+    }
+}
