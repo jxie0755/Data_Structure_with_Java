63b6f4eb4423dcff6e8a3585e7d09956796ac464
jxie0755 <jxie0755@users.noreply.github.com>
2019-03-09 23:37:52 -0500

    Learn generics

--- Java_Simple_Learnings/zlearnings/Generics_Statement.java
+++ Java_Simple_Learnings/zlearnings/Generics_Statement.java
@@ -1,3 +1,5 @@
+package zlearnings;
+
 import java.util.List;
 
 // This is to learn the difference of the generic type claim
@@ -16,19 +18,19 @@ public class Generics_Statement {
         Car c3 = new Car();
 
         List<Car> carList = List.of(c1,c2,c3);
-        System.out.println(carList.get(0).getClass());      // >>> class Car
+        System.out.println(carList.get(0).getClass());      // >>> class zlearnings.Car
         System.out.println(carList.get(0) instanceof Car);  // >>> true
 
         carList.get(0).foo();
 
         List<Object> objList = List.of(c1,c2,c3);
-        System.out.println(objList.get(0).getClass());      // >>> class Car
+        System.out.println(objList.get(0).getClass());      // >>> class zlearnings.Car
         System.out.println(objList.get(0) instanceof Car);  // >>> true
         // objList.get(0).foo();
 
 
         List rawList = List.of(c1,c2,c3);
-        System.out.println(rawList.get(0).getClass());      // >>> class Car
+        System.out.println(rawList.get(0).getClass());      // >>> class zlearnings.Car
         System.out.println(rawList.get(0) instanceof Car);  // >>> true
         // rawList.get(0).foo();
 
