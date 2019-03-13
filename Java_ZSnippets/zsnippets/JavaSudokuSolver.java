3b610ffa91226e00ec758d3ca8ce51489b85daa7
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-03-13 15:46:19 -0400

    Build Java Sudoku Solver

---
+++ Java_ZSnippets/zsnippets/JavaSudokuSolver.java
@@ -0,0 +1,14 @@
+package zsnippets;
+
+
+/**
+ * This is to create the same sudoku solver algorithm from python version
+ *  from: sudoku_solver_v3.py by using HashMap
+ **/
+
+public class JavaSudokuSolver {
+
+
+
+}
+
