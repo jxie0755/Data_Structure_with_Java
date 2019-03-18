bd18aac6e018489504b6a83c4acec25fc105a1bd
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-03-18 12:54:47 -0400

    Learn use of this

--- Java_Simple_Learnings/zlearnings/Use_of_This.java
+++ Java_Simple_Learnings/zlearnings/Use_of_This.java
@@ -5,6 +5,32 @@ public class Use_of_This {
 }
 
 
+// 不像python强制使用self, java可以选择性不使用this
+class Point1 {
+    public int x = 0;
+    public int y = 0;
+
+    //constructor
+    public Point1(int a, int b) {
+        x = a;
+        y = b;
+    }
+}
+
+// but it could have been written like this:
+class Point2 {
+    public int x = 0;
+    public int y = 0;
+
+    //constructor
+    public Point2(int x, int y) {
+        this.x = x;   // 区分开内部参数和外部参数
+        this.y = y;
+    }
+}
+
+
+
 class Father {
 
     // 当一个类有多个Constructor的时候, 重载Constructor可以用this来省略一些代码
