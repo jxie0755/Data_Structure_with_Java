b7610b368b0012cdee3e5ca37fed4b0ad471ab61
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-03-18 21:50:44 -0400

    test max recursion depth

--- Java_Simple_Learnings/zlearnings/max_recursion.java
+++ Java_Simple_Learnings/zlearnings/max_recursion.java
@@ -7,8 +7,24 @@ public class max_recursion {
         maxRecursionDepth(n+1);
     }
 
+    static int fib(int n) {
+        if (n < 2) {
+            return n;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+
     public static void main(String[] args) {
-        maxRecursionDepth(1);
+        // maxRecursionDepth(1);
+
+        double startTime  =System.currentTimeMillis();   //获取开始时间
+
+        System.out.println(max_recursion.fib(45));
+
+        double endTime = System.currentTimeMillis(); //获取结束时间
+        System.out.println("--- "+ (endTime-startTime)/1000  + "s seconds ---");
+
     }
 }
 
