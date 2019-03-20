c823c9b18c58c897a730168cc8688d14b85cd43a
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-03-20 08:57:17 -0400

    Learn use of final

--- Java_101/Java_OOP/C00d_Use_of_Final.java
+++ Java_101/Java_OOP/C00d_Use_of_Final.java
@@ -3,16 +3,25 @@ package Java_OOP;
 public class C00d_Use_of_Final {
 
     /*
-     * final修饰的变量即成为常量， 只能赋值一次， 但是final所修饰局部变量和成员变量有所不同
-        * final修饰的局部变量必须使用之前被赋值一次才能使用
-        * final修饰的成员变量在声明时没有赋值的叫“空白final变量”
-            * 空白final变量必须在构造方法或静态代码块中初始化。
+     * final
+        * final修饰的变量即成为常量， 只能赋值一次， 但是final所修饰局部变量和成员变量有所不同
+            * final修饰的局部变量必须使用之前被赋值一次才能使用
+            * final修饰的成员变量在声明时没有赋值的叫“空白final变量”
+                * 空白final变量必须在构造方法或静态代码块中初始化。
+
+        * final修饰的类不能被继承。
+            * 有时出于设计安全的目的， 不想让自己编写的类被别人继承， 这时可以使用final关键字修饰父类
+
+        * final修饰的方法不能被子类覆盖。
+            * 有时也是出于设计安全的目的， 父类中的方法不想被别人覆盖， 这是可以使用final关键字修饰父类中方法
      */
 
 }
 
 
-class FinalDemo {
+class FinalVariable {
+
+    // final修饰变量
 
     void doSomething() {
 
@@ -40,8 +49,31 @@ class FinalDemo {
         d = 32;   // 只能赋值一次
     } // 构造方法
 
-    FinalDemo() {
+    FinalVariable() {
         // 初始化实例变量
         b = 3;   // 只能赋值一次
     }
+}
+
+
+final class FinalClass {
+    // final修饰类
+}
+
+// class SubFinalClass extends FinalClass {
+//     //编译错误
+// }
+
+
+class SuperClass {
+    final void doSomeThing() {
+        System.out.println("WTF?");
+    }
+}
+
+class SubSuperClass extends  SuperClass {
+    // @Override
+    // void doSomeThing() {
+    //     System.out.println("What's up?");  // 编译错误
+    // }
 }
