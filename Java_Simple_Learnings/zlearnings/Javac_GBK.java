f07917435ec556b75f411fd169ef253af14ccd82
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-03-22 08:48:37 -0400

    update

--- Java_Simple_Learnings/zlearnings/Javac_GBK.java
+++ Java_Simple_Learnings/zlearnings/Javac_GBK.java
@@ -1,6 +1,6 @@
-package Java_Basics;
+package zlearnings;
 
-public class C12_Javac_GBK {
+public class Javac_GBK {
     // 编译带有中文注释的java文件可能出错 (IDE可能不会,但是console里直接JDK是会的)
     // 解决方案:
     // 执行 javac -encoding utf-8 XXX.java 以取代 javac XXX.java
