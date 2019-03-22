e707081f9f683e81d53d9304db206e28bf4dbbb0
jxie0755 <jxie0755@users.noreply.github.com>
2019-03-22 19:54:00 -0400

    Learn lambda and inner class

--- Java_101/Java_OOP/C09_Inner_Class.java
+++ Java_101/Java_OOP/C09_Inner_Class.java
@@ -16,18 +16,20 @@ public class C09_Inner_Class {
             * 提供命名空间.      静态内部类和外部类能够提供有别于包的命名空间
             * 便于访问外部类成员. 内部类能够很方便访问所在外部类的成员, 包括私有成员也能访问。
 
+
         * 内部类分类
+
             * 有名内部类
+                * 成员内部类  -- 成员内部类类似于外部类的成员变量， 在外边类的内部， 且方法体和代码块之外定义的内部类
+                    * 实例成员内部类  -- 可以访问外部类的各种成员
+                    * 静态成员内部类  -- 只能访问外部类的静态成员(方法/属性)
+
                 * 局部内部类
                     * 局部内部类就是在方法体或代码块中定义的内部类, 局部内部类的作用域仅限于方法体或代码块中。
                     * 局部内部类访问级别只能是默认的, 不能是公有的, 私有的和保护的访问级别，
                         * 局部内部类也不能是静态， 即不能使用static修饰。
                         * 局部内部类可以访问外部类所有成员
                         * 局部内部类可以出现在静态和费静态方法中, 如果在静态方法中也同样只能访问外部类的静态成员!
-
-                * 成员内部类  -- 成员内部类类似于外部类的成员变量， 在外边类的内部， 且方法体和代码块之外定义的内部类
-                    * 实例成员内部类  -- 可以访问外部类的各种成员
-                    * 静态成员内部类  -- 只能访问外部类的静态成员(方法/属性)
 
             * 匿名内部类
                 * 匿名内部类是没有名字的内部类, 本质上是没有名的局部内部类, 具有局部内部类所有特征。
@@ -40,6 +42,7 @@ public class C09_Inner_Class {
 
 
 
+// 实例成员内部类实例:
 class Outer1 {
 
     private String x = "OUT";  // Outer1变量
@@ -59,14 +62,14 @@ class Outer1 {
         inner.display();
     }
 
-    // 实例成员内部类
+    // 建立实例成员内部类
     class Inner {
         private String x = "Inner"; // Inner变量
 
         void display() { // Inner方法
             Outer1.this.foo();
             Outer1.bar();
-            // 在内部类中this是引用当前内部类对象， 见代码第⑤行。 而要引用外部类对象需要使用“外部类名.this
+            // 在内部类中this是引用当前内部类对象。 而要引用外部类对象需要使用“外部类名.this
             System.out.println(Outer1.this.x + " " + Outer1.y);  // 如果命名不冲突,可以省略Outer1.this
             System.out.println(this.x);
             System.out.println(x);
@@ -104,7 +107,7 @@ class Outer1 {
 }
 
 
-
+// 静态成员内部类实例:
 class Outer2 {
 
     private String x = "OUT";  // Outer2变量
@@ -118,7 +121,7 @@ class Outer2 {
         System.out.println("barrrr");
     }
 
-    // 静态成员内部类
+    // 建立静态成员内部类
     static class Inner {
 
         void onClick() {
@@ -139,24 +142,24 @@ class Outer2 {
 }
 
 
-
+// 局部内部类实例
 class Outer3 {
 
     private String x = "OUT";  // Outer2变量
     private static String staticX = "static OUT"; //  Outer2静态变量
 
-    void shoot() {
+    void shoot3() {
         System.out.println("SHOOT!");
     }
 
     void foo(int x, int y) {
         int z = 100;
 
-        // 局部内部类
+        // 建立局部内部类
         class Inner {
             void display() {
                 int summ = x + y + z;
-                Outer3.this.shoot();
+                Outer3.this.shoot3();
                 System.out.println(Outer3.this.x);
                 System.out.println(Outer3.staticX);
                 System.out.println(summ);
@@ -175,7 +178,7 @@ class Outer3 {
         class Inner {  // 可以重名, 因为它被局限在一个方法内部了
             void display() {
                 int summ = x + y + z;
-                // Outer3.this.shoot();
+                // Outer3.this.shoot3();
                 // System.out.println(Outer3.this.x);
                 System.out.println(Outer3.staticX);
                 System.out.println(summ);
@@ -214,12 +217,13 @@ interface Inner4 {   // 建立一个inner4 接口 // 也可以是class这里无�
     void display();
 }
 
+// 匿名内部类实例
 class Outer4 {
 
     private String x = "OUT";  // Outer2变量
     private static String staticX = "static OUT"; //  Outer2静态变量
 
-    void shoot(Inner4 inn) { // 参数为Inner4 实例, 但是需要一个子类实例化这个接口
+    void shoot4(Inner4 inn) { // 参数为Inner4 实例, 但是需要一个子类实例化这个接口
         inn.display();
     }
 
@@ -229,9 +233,10 @@ class Outer4 {
 
         Outer4 out4 = new Outer4();
         // 方法参数是匿名内部类
-        out4.shoot(new Inner4() {  // 实际上是new了一个Inner4实例在内部, 直接重写其display方法
+        out4.shoot4(new Inner4() {  // 实际上是new了一个Inner4实例在内部, 直接重写其display方法
                                    // 因为new出来没有给名字所以匿名
                                    // 匿名类可以直接实例化接口和抽象类,不必在乎接口和抽象类没有具体子类
+                                   // 从这里就可以引出lambda函数的思想
             @Override
             public void display(){
                 System.out.println("来自匿内部类!!");
@@ -243,11 +248,11 @@ class Outer4 {
         // 继承类的匿名内部类(具体类作为内部类)
         Outer3 out33 = new Outer3(){  // 实际上是重写了一个Outer3在这个类的内部
             @Override
-            public void shoot(){
+            public void shoot3(){
                 System.out.println("Shoot from Outer 4!!");
             };
         };
-        out33.shoot();
+        out33.shoot3();
         // >>> Shoot from Outer 4!!
         // 使用时直接用这个实例, 所以这个out33的类名不再重要
         // 匿名内部类通常用来实现接口或抽象类的， 很少覆盖具体类
