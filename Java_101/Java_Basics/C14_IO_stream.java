906458a1dad890ade56b7a65aa12f3c21c2d4d61
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-03-27 15:00:11 -0400

    Learn stream

--- Java_101/Java_Basics/C14_IO_stream.java
+++ Java_101/Java_Basics/C14_IO_stream.java
@@ -13,7 +13,13 @@ public class C14_IO_stream {
         * 输出流 - 将数据写入到目的地是输出流
             * 数据输出的目的地也有多种形式, 如文件, 网络和控制台, 控制台(console)是默认的标准输出设备
      * 所有的输入形式都抽象为输入流， 所有的输出形式都抽象为输出流， 它们与设备无关
-     * 以字节为单位的流称为字节流， 以字符为单位的流称为字符流。
+
+     * 字节流处理所有二进制文件(所有电脑文件), 字符流只处理文本文件.
+         * 以字节为单位的流称为字节流
+            * Java中的字节流处理的最基本单位为单个字节，它通常用来处理二进制数据 (也就是处理所有文件)
+         * 以字符为单位的流称为字符流
+            * 通常用来处理文本数据。所谓Unicode码元，也就是一个Unicode代码单元，范围是0x0000~0xFFFF
+            * 在以上范围内的每个数字都与一个字符相对应，Java中的String类型默认就把字符以Unicode规则编码而后存储在内存中
 
      * Java SE提供4个顶级抽象类:
         * 两个字节流抽象类:
@@ -25,9 +31,14 @@ public class C14_IO_stream {
                 * 实体子类 FileOutputStream
                 * 实体子类 FilterOutputStream
                     * 实体子类 BufferedOutputStream
-        * 两个字符流抽象类
+
+        * 两个字符流抽象类 (基本对应字节流的架构)
             * Reader
+                * 实体子类 FileReader
+                * 实体子类 BufferedReader
             * Writer
+                * 实体子类 FileWriter
+                * 实体子类 BufferedWriter
      */
 
 
@@ -189,16 +200,12 @@ class Java_Byte_Stream {
                 // 如果剩下不到10个新鲜数据, 比如只有2个,就会返回2
                 // 如果再往下, 没有任何新鲜数据, 才返回-1
 
-
-                String copyStr = new String(buffer);
                 // 这个String会把整个buffer读出来, 问题是,buffer可能不全是新鲜数据
+                System.out.println(new String(buffer));
 
-                System.out.println(copyStr);
-                // 开始写入
-                out.write(buffer, 0, len);
                 // 这样写就可以保证只把新鲜数据写到新文件, 而不是整个String
-
-                len = in.read(buffer);  // 刷新len
+                out.write(buffer, 0, len);  // 开始写入
+                len = in.read(buffer);         // 刷新len
             }
 
         } catch (FileNotFoundException e) {
@@ -217,23 +224,17 @@ class Java_Byte_Stream {
              BufferedInputStream bis = new BufferedInputStream(in2);
              BufferedOutputStream bos = new BufferedOutputStream(out2)) {
 
-            //开始时间
-            long startTime = System.nanoTime();
-
-            // 准备一个缓冲区
-            byte[] buffer = new byte[1024];
-            // 首先读取一次
-            int len = bis.read(buffer);
+            long startTime = System.nanoTime();  //开始时间
+            byte[] buffer = new byte[1024];      // 准备一个缓冲区
+            int len = bis.read(buffer);          // 首先读取一次
 
             while (len != -1) {
-                // 开始写入数据
-                bos.write(buffer, 0, len);
-                // 再读取一次
-                len = bis.read(buffer);
+                bos.write(buffer, 0, len);   // 开始写入数据
+                len = bis.read(buffer);          // 再读取一次
             }
-            //结束时间
-            long elapsedTime = System.nanoTime() - startTime;
+            long elapsedTime = System.nanoTime() - startTime; //结束时间
             System.out.println("buffered 耗时： " + (elapsedTime / 1000000.0) + " 毫秒");
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -243,23 +244,18 @@ class Java_Byte_Stream {
         try (FileInputStream in3 = new FileInputStream("./TestDir/testV.mp4");
              FileOutputStream out3 = new FileOutputStream("./TestDir/testV_out.mp4");){
 
-            //开始时间
-            long startTime = System.nanoTime();
-
-            // 准备一个缓冲区
-            byte[] buffer = new byte[1024];
-            // 首先读取一次
-            int len = in3.read(buffer);
+            long startTime = System.nanoTime(); //开始时间
+            byte[] buffer = new byte[1024];     // 准备一个缓冲区
+            int len = in3.read(buffer);        // 首先读取一次
 
             while (len != -1) {
-                // 开始写入数据
-                out3.write(buffer, 0, len);
-                // 再读取一次
-                len = in3.read(buffer);
+                out3.write(buffer, 0, len);   // 开始写入数据
+                len = in3.read(buffer);           // 再读取一次
             }
             //结束时间
             long elapsedTime = System.nanoTime() - startTime;
             System.out.println("not buffered 耗时： " + (elapsedTime / 1000000.0) + " 毫秒");
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -269,9 +265,184 @@ class Java_Byte_Stream {
         // buffered 耗时： 2644.297795 毫秒        (快很多)
         // not buffered 耗时： 11545.606955 毫秒
 
-        // 如果缓冲区设为32768:  // 则都更快,甚至差距很小
-        // buffered 耗时： 1383.841018 毫秒
-        // not buffered 耗时： 1347.625697 毫秒
+        // 如果缓冲区设为4096:
+        // 则buffer开始变慢, non-buffer变快, 也就是说如果缓冲区可以足够大, 则不需要用缓冲字节流
+        // buffered 耗时： 3137.131243 毫秒
+        // not buffered 耗时： 4267.921515 毫秒
+    }
+}
+
+class Java_Character_Stream {
+
+    /*
+     * Module java.base
+     * Package java.io
+     * Class Reader    (Abstract)
+     */
+
+    /*
+     * Reader主要方法
+        * int read()
+            * 读取一个字符， 返回值范围在0~65535(0x00~0xffff)之间。
+            * 如果因为已经到达流末尾， 则返回值-1。
+
+        * int read(char[] cbuf)
+            * 将字符读入到数组cbuf中， 返回值为实际读取的字符的数量
+            * 如果因为已经到达流末尾， 则返回值-1。
+
+        * int read(char[] cbuf, int off, int len)
+            * 最多读取len个字符， 数据放到以下标off开始字符数组cbuf中， 将读取的第一个字符存储在元素cbuf[off]中，
+            * 下一个存储在cbuf[off+1]中， 依次类推。 返回值为实际读取的字符的数量，
+            * 如果因为已经到达流末尾， 则返回值-1。
+
+        * void close()
+            * 流操作完毕后必须关闭。
+
+        * 上述所有方法都声明了抛出IOException， 因此使用时要注意处理异常
+
+     * FileReader 构造方法
+        * FileReader(String fileName)
+            * 创建FileReader对象， fileName是文件名。
+            * 如果文件不存在则抛出FileNotFoundException异常。
+
+        * FileReader(File file)
+            * 通过File对象创建FileReader对象。
+            * 如果文件不存在则抛出FileNotFoundException异常
+
+     * BufferedReader
+        * 构造方法
+            * BufferedReader(Reader in)
+                * 通过一个底层输入流in对象创建缓冲流对象， 缓冲区大小是默认的， 默认值8192。
+
+            * BufferedReader(Reader in, int size)
+                * 通过一个底层输入流in对象创建缓冲流对象
+                * size指定的缓冲区大小， 缓冲区大小应该是2的n次幂， 这样可提高缓冲区的利用率。
+        * 特殊方法
+            * String readLine()
+                * 读取一个文本行， 如果因为已经到达流末尾， 则返回值null
+     */
+
+
+
+    /*
+     * Module java.base
+     * Package java.io
+     * Class Writer    (Abstract)
+     */
+
+    /*
+     * Writer主要方法
+        * void write(int c)
+            * 将整数值为c的字符写入到输出流， c是int类型占有32位
+            * 写入过程是写入c的16个低位， c的16个高位将被忽略。
+
+        * void write(char[] cbuf)
+            * 将字符数组cbuf写入到输出流。
+
+        * void write(char[] cbuf, int off, int len)
+            * 把字符数组cbuf中从下标off开始， 长度为len的字符写入到输出流。
+
+        * void write(String str)
+            * 将字符串str中的字符写入输出流。
+
+        * void write(String str,int off,int len)
+            * 将字符串str 中从索引off开始处的len个字符写入输出流。
+
+        * void flush()
+            * 刷空输出流， 并输出所有被缓存的字符。
+            * 由于某些流支持缓存功能， 该方法将把缓存中所有内容强制输出到流中。
+
+        * void close( )
+            * 流操作完毕后必须关闭。
+
+        * 上述所有方法都可以会抛出IOException， 因此使用时要注意处理异常。
+
+     * FileWriter 构造方法
+        * FileWriter(String fileName)
+            * 通过指定fileName文件名创建FileWriter对象。 如果fileName文件存在，
+            * 但如果是一个目录或文件无法打开则抛出FileNotFoundException异常。
+
+        * FileWriter(String fileName, boolean append)
+            * 通过指定fileName文件名创建FileWriter对象
+            * append参数如果为 true， 则将字符写入文件末尾处， 而不是写入文件开始处。
+            * 如果fileName文件存在， 但如果是一个目录或文件无法打开则抛出FileNotFoundException异常。
+
+        * FileWriter(File file)
+            * 通过File对象创建FileWriter对象。
+            * 如果file文件存在， 但如果是一个目录或文件无法打开则抛出FileNotFoundException异常。
+
+        * FileWriter(File file, boolean append)
+            * 通过File对象创建FileWriter对象
+            * append参数如果为 true， 则将字符写入文件末尾处， 而不是写入文件开始处。
+            * 如果file文件存在， 但如果是一个目录或文件无法打开则抛出FileNotFoundException异常。
+
+     * BufferedWriter
+        * 构造方法
+            * BufferedWriter(Writerout)
+                * 通过一个底层输出流out 对象创建缓冲流对象
+                * 缓冲区大小是默认的， 默认值8192。
+            * BufferedWriter(Writerout, int size)
+                * 通过一个底层输出流out对象创建缓冲流对象
+                * size指定的缓冲区大小， 缓冲区大小应该是2的n次幂， 这样可提高缓冲区的利用率。
+        * 特殊方法
+            * void newLine()
+                * 写入一个换行符
+     */
+
+    public static void main(String[] args) {
+
+        // 同样的方法复制py文档
+        try (FileReader in = new FileReader("./TestDir/hello.py");
+             FileWriter out = new FileWriter("./TestDir/hello_out.py")) {
+
+            // 准备一个缓冲区
+            char[] buffer = new char[4];
+            // 首先读取一次
+            int len = in.read(buffer);
+            while (len != -1) {
+                 System.out.println("len: " + len);
+                // 注意这个len才是真正表示读了几个in, 如果设定缓冲区为10
+                // 那么每次如果可以全部读取新鲜数据就会返回
+                // 如果剩下不到10个新鲜数据, 比如只有2个,就会返回2
+                // 如果再往下, 没有任何新鲜数据, 才返回-1
+
+                // 这个String会把整个buffer读出来, 问题是,buffer可能不全是新鲜数据
+                System.out.println(new String(buffer));
+
+                // 这样写就可以保证只把新鲜数据写到新文件, 而不是整个String
+                out.write(buffer, 0, len);  // 开始写入
+                len = in.read(buffer);          // 刷新len
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        // 使用缓冲字符流复制文本文件也是基本一样的操作
+        try (FileReader in2 = new FileReader("./TestDir/hello.py");
+             FileWriter out2 = new FileWriter("./TestDir/hello_buffered_out.py");
+             BufferedReader bis = new BufferedReader(in2);
+             BufferedWriter bos = new BufferedWriter(out2)) {
+
+            // 首先读取一行文本
+            String line = bis.readLine();
+
+            while (line != null) {
+                bos.write(line);       // 开始写入数据
+                bos.newLine();         // 写一个换行符
+                line = bis.readLine(); // 再读取一行文本
+            }
+            System.out.println("复制完成");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
 
