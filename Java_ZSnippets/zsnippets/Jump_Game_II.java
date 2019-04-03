f2e24f3bf2fd5eeb8296e9444b68f0e2db5b3e5d
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-04-03 09:30:56 -0400

    Practice inner class by Leetcode p45

--- Java_ZSnippets/zsnippets/Jump_Game_II.java
+++ Java_ZSnippets/zsnippets/Jump_Game_II.java
@@ -1,9 +1,6 @@
 package zsnippets;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Jump_Game_II {
 
@@ -11,15 +8,25 @@ public class Jump_Game_II {
 
 
     Integer jump(List<Integer> nums) {
+
         Integer last_idx = nums.size() - 1;
         List<Integer> all_ways = new ArrayList<>();
 
+        Map<Integer, Double> hmp = new HashMap<>();
+        for (int i = 0; i < nums.size(); i += 1) {
+            hmp.put(i, Double.POSITIVE_INFINITY);
+        }
+
+        // 需要注意的是, 内部类不是先被确定存在, 运行时才查看变量位置, 而是一开始就确定所有变量
+        // 所以, 外部类中的先决变量必须放在这个内部类定义之前 (内部类的位置不能任意摆放)
         class JumpHelp {
             void helper(Integer cur_idx, Integer cur_step) {
-                if (cur_idx >= last_idx) {
-                    all_ways.add(cur_step);
-                } else {
-                    Integer cur_value = nums.get(cur_idx);
+                Integer cur_value = nums.get(cur_idx);
+
+                if (cur_value >= last_idx - cur_idx) {
+                    all_ways.add(cur_step+1);
+                } else if (cur_step < hmp.get(cur_idx)){
+                    hmp.replace(cur_idx, (double) cur_step);
                     for (int i = 1; i <= cur_value; i += 1) {
                         this.helper(cur_idx + i, cur_step + 1);
                     }
@@ -27,18 +34,23 @@ public class Jump_Game_II {
             }
         }
 
+
         JumpHelp inner = new JumpHelp();
         inner.helper(0, 0);
-        return Collections.min(all_ways);
-    }
 
+        if (nums.size() <= 1) {
+            return 0;
+        }
+        return Collections.min(all_ways);
+
+    }
 
 
     public static void main(String[] args) {
         System.out.println(new Jump_Game_II().jump(new ArrayList<>(Arrays.asList(2,1))));  // 1
         System.out.println(new Jump_Game_II().jump(new ArrayList<>(Arrays.asList(2,3,1,1,4)))); // 2
         System.out.println(new Jump_Game_II().jump(new ArrayList<>(Arrays.asList(5,6,5,3,9,8,3,1,2,8,2,4,8,3,9,1,0,9,4,6,5,9,8,7,4,2,1,0,2)))); // 5
-        // System.out.println(new Jump_Game_II().jump(new ArrayList<>(Arrays.asList(5,6,4,4,6,9,4,4,7,4,4,8,2,6,8,1,5,9,6,5,2,7,9,7,9,6,9,4,1,6,8,8,4,4,2,0,3,8,5)))); // 5
+        System.out.println(new Jump_Game_II().jump(new ArrayList<>(Arrays.asList(5,6,4,4,6,9,4,4,7,4,4,8,2,6,8,1,5,9,6,5,2,7,9,7,9,6,9,4,1,6,8,8,4,4,2,0,3,8,5)))); // 5
 
 
     }
