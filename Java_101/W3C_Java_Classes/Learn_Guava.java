281a60d93ab673d145aaa85e05307b16c918d09d
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-04-03 16:59:47 -0400

    Learn guava

---
+++ Java_101/W3C_Java_Classes/Learn_Guava.java
@@ -0,0 +1,12 @@
+package W3C_Java_Classes;
+
+import com.google.common.collect.Lists;
+import java.util.List;
+
+public class Learn_Guava {
+    public static void main(String[] args) {
+        List<String> lst = Lists.newArrayList("a", "b", "c", "d");
+        System.out.println(lst);
+    }
+}
+
