1f1b543a091ab8b15160ffddfd5f4c32b8c77a30
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-06-22 22:43:14 -0400

    Initiate Lecture 07 Learn binary heap

---
+++ Algorithms/algorithm_p1/week_4/Alg_Lecture_07_APIs_and_Elementary_Implementations.java
@@ -0,0 +1,70 @@
+package algorithm_p1.week_4;
+
+public class Alg_Lecture_07_APIs_and_Elementary_Implementations {
+
+    /*
+     * Collections
+        * Stack - Remove the item most recently added
+        * Queue - Remove the item least recently added
+        * Randomized queue - Remove a random item
+        * Priority queue - Remove the largest (or smallest) item
+            * Use Binary Heap (tree like) -- the best method
+                * but use array to implement
+                    * index 0 is max/min
+                    * then children is always smaller/larger than parent
+                    * finding parent index of children at index k is easy: k // 2
+                    * finding left and right of a parent at k is easy: k*2 and k*2+1
+                * Can be setup in two ways:
+                    * Top is the largest (Max-oriented)
+                    * Top is the smallest (Min-oriented)
+                * Rest of the sequence follow the same rule, but it left and right does not matter
+                * Exchange is needed to revise, push and pop values from the binary-heap
+                    * repeat until heap order restored
+     */
+
+}
+
+
+class BinaryHeap_maxOriented {
+
+    // Initiation
+    private Comparable[] heap;
+    private int N;
+
+    public BinaryHeap_maxOriented (Comparable[] a) {
+
+        N = a.length;
+        heap = this.heapify(a);
+    }
+
+    private Comparable[] heapify(Comparable[] a) {
+        return a;
+    }
+
+    // Utility functions
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+
+    // Operation
+    private void sink(int k) {
+        // sink idx at k in a binary heap in the form of Comparable[]
+        // Restore the order of the binary heap
+
+        while (2 * k <= N) {
+
+        }
+
+    }
+
+
+
+
+}
