58d74ae873af797b3886bed9444e1d16a309f89a
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-06-24 19:53:26 -0400

    Update Leetcode 084

--- Leetcode/LC084_Solution.java
+++ Leetcode/LC084_Solution.java
@@ -3,6 +3,14 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+
+/**
+ * P084 Largest Rectangle in Histogram
+ * Hard
+ *
+ * Given n non-negative integers representing the histogram's bar height where the width of each bar is 1,
+ * find the area of largest rectangle in the histogram.
+ */
 public class LC084_Solution {
 
     public int largestRectangleArea(int[] heights) {
