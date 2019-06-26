7acbd39446d2122e7736fe652a4139c86564a74d
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-06-26 11:51:12 -0400

    Finish Leetcode 002

--- Leetcode/LC002_Add_Two_Numbers.java
+++ Leetcode/LC002_Add_Two_Numbers.java
@@ -11,15 +11,59 @@
  */
 public class LC002_Add_Two_Numbers {
 
-    // public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    //
-    // }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int val = carry;
+            if (l1 != null) {
+                val += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                val += l2.val;
+                l2 = l2.next;
+            }
+            carry = val / 10;
+            val = val % 10;
+
+            current.next = new ListNode(val);
+            current = current.next;
+        }
+        if (carry == 1) {
+            current.next = new ListNode(1);
+        }
+
+        return dummy.next;
+    }
 
 
     public static void main(String[] args) {
 
+        ListNode a1 = ListNode.genNode(new int[]{0});
+        ListNode b1 = ListNode.genNode(new int[]{0, 1});
+        ListNode c1 = new LC002_Add_Two_Numbers().addTwoNumbers(a1, b1);
+        assert ListNode.showString(c1).equals("0->1"): "Simple Case 1";
 
+        ListNode a2 = new ListNode(9);
+        ListNode b2 = new ListNode(9);
+        ListNode c2 = new LC002_Add_Two_Numbers().addTwoNumbers(a2, b2);
+        assert ListNode.showString(c2).equals("8->1"): "Simple Case 2";
 
+        ListNode a3 = ListNode.genNode(new int[]{2, 4, 3});
+        ListNode b3 = ListNode.genNode(new int[]{5, 6, 4});
+        ListNode c3 = new LC002_Add_Two_Numbers().addTwoNumbers(a3, b3);
+        assert ListNode.showString(c3).equals("7->0->8") : "Example 1";
+
+        ListNode a4 = ListNode.genNode(new int[]{2, 4, 3});
+        ListNode b4 = ListNode.genNode(new int[]{8, 9});
+        ListNode c4 = new LC002_Add_Two_Numbers().addTwoNumbers(a4, b4);
+        assert ListNode.showString(c4).equals("0->4->4"): "Example 2";
+
+        System.out.println("all passed");
     }
 }
 
