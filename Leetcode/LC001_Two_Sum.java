9923b6986b12d09e9bc335a70bd41af9fffd0320
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-06-27 16:00:53 -0400

    Update

--- Leetcode/LC001_Two_Sum.java
+++ Leetcode/LC001_Two_Sum.java
@@ -9,7 +9,6 @@ import java.util.Map;
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
  */
-
 public class LC001_Two_Sum {
 
     public int[] twoSum(int[] nums, int target) {
