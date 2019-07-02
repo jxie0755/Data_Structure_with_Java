d72a336f0258f6f1d41245d2ac4d129be397e067
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-07-02 15:30:34 -0400

    Leetcode P007 math method finished

--- Leetcode/LC007_Reverse_Integer.java
+++ Leetcode/LC007_Reverse_Integer.java
@@ -4,24 +4,24 @@ public class LC007_Reverse_Integer {
      * Version A, use string method, time O(logN), takes extra space O(LogN)
      * This is not very fast as it convert types back and forth
      */
-    public int reverse(int x) {
+    public int reverse_string(int x) {
 
-        StringBuilder result = new StringBuilder();
+        StringBuilder result_string = new StringBuilder();
         String current = String.valueOf(x);
 
         Long ans;  // Handle the integer overflow by create ans number in Long first
 
         if (x >= 0) {
             for (int i = current.length() - 1; i >= 0; i -= 1) {
-                result.append(current.charAt(i));
+                result_string.append(current.charAt(i));
             }
-            ans = Long.valueOf(result.toString());  // Direct convert string to Long
+            ans = Long.valueOf(result_string.toString());  // Direct convert string to Long
 
         } else {
             for (int i = current.length() - 1; i > 0; i -= 1) {
-                result.append(current.charAt(i));
+                result_string.append(current.charAt(i));
             }
-            ans = Long.valueOf(result.toString()); // Direct convert string to Long
+            ans = Long.valueOf(result_string.toString()); // Direct convert string to Long
         }
 
         if (ans <= 2147483647) {  // assure no overlfow integer
@@ -32,9 +32,31 @@ public class LC007_Reverse_Integer {
             }
         }
         return 0;
-
     }
 
+
+    /**
+     * Version B, use math method
+     */
+    public int reverse(int x) {
+
+        int absX = Math.abs(x);
+        Long result = 0L;
+
+        while (absX > 0) {
+            result = result * 10 + absX % 10;
+            absX /= 10;
+        }
+
+        if (x < 0) {
+            result *= -1;
+        }
+
+        if (-2147483647 < result && result < 2147483647) {
+            return result.intValue();
+        }
+        return 0;
+    }
 
 
 
