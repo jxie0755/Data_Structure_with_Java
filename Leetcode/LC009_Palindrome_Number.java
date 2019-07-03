a81a10e9884f4a15940fe20ba91eb025954b7ed8
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-07-03 09:35:36 -0400

    Leetcode P009 math method finished

---
+++ Leetcode/LC009_Palindrome_Number.java
@@ -0,0 +1,39 @@
+/**
+ * p009 Palindrome Number
+ * Easy
+ *
+ * Determine whether an integer is a palindrome. Do this without extra space.
+ * Negative integer will not be a palindrome
+ */
+public class LC009_Palindrome_Number {
+
+    /**
+     * Version B, divmod method
+     */
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int copy = x;
+        int reverse = 0;
+        while (copy > 0) {
+            reverse = reverse * 10 + copy % 10;
+            copy /= 10;
+        }
+        return reverse == x;
+    }
+
+    public static void main(String[] args) {
+
+        assert new LC009_Palindrome_Number().isPalindrome(21477412) : "Is palindrome even";
+        assert new LC009_Palindrome_Number().isPalindrome(12321) : "Is palindrome odd";
+        assert ! new LC009_Palindrome_Number().isPalindrome(1477412) : "Not palindrome";
+        assert new LC009_Palindrome_Number().isPalindrome(1) : "Single 1";
+        assert new LC009_Palindrome_Number().isPalindrome(0) : "Single 0";
+
+        System.out.println("all passed");
+
+    }
+}
+
