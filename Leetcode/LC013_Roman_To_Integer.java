67f2e4c891237dfab32b0bb56e27175734afc661
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-07-06 17:49:19 -0400

    Leetcode P013 finished

---
+++ Leetcode/LC013_Roman_To_Integer.java
@@ -0,0 +1,67 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * p013 Roman to Integer
+ * Easy
+ *
+ * Given a roman numeral, convert it to an integer.
+ * Input is guaranteed to be within the range from 1 to 3999.
+ */
+public class LC013_Roman_To_Integer {
+
+
+    static Map<String, Integer> checklist = new HashMap<>();
+    static {
+        checklist.put("CM", 900);
+        checklist.put("CD", 400);
+        checklist.put("XC", 90);
+        checklist.put("XL", 40);
+        checklist.put("IX", 9);
+        checklist.put("IV", 4);
+    }
+
+    static Map<String, Integer> Roman_Nu  = new HashMap<>();
+    static {
+        Roman_Nu.put("M", 1000);
+        Roman_Nu.put("D", 500);
+        Roman_Nu.put("C", 100);
+        Roman_Nu.put("L", 50);
+        Roman_Nu.put("X", 10);
+        Roman_Nu.put("V", 5);
+        Roman_Nu.put("I", 1);
+    }
+
+    /**
+     * Version A, hashmap method
+     */
+    public int romanToInt(String s) {
+        int result = 0;
+
+
+        for (Map.Entry<String, Integer> entry : checklist.entrySet()) {
+            if (s.contains(entry.getKey())) {
+                String k = entry.getKey();
+                Integer v = entry.getValue();
+                result += v;
+                s = s.replaceFirst(k, "");
+            }
+        }
+
+        for (int i = 0; i < s.length(); i += 1) {
+            String sub = s.substring(i, i + 1);
+            result += Roman_Nu.get(sub);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        assert new LC013_Roman_To_Integer().romanToInt("MMMCDXCIX") == 3499: "Example 1";
+        System.out.println("all passed");
+
+    }
+
+}
+
