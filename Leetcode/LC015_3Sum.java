7183afc55d577c507e195f1dac7953b596657e60
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-07-08 16:29:13 -0400

    Leetcode 015 Initiation

---
+++ Leetcode/LC015_3Sum.java
@@ -0,0 +1,23 @@
+/**
+ * P015 3Sum
+ * Medium
+ *
+ * Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?
+ * Find all unique triplets in the array which gives the sum of zero.
+ *
+ * Note: The solution set must not contain duplicate triplets.
+ */
+public class LC015_3Sum {
+
+    // public List<List<Integer>> threeSum(int[] nums) {
+    //
+    // }
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
+
