4fc2c9656da4c3912d33b07a868f3e8aea693c04
jxie0755 <jxie0755@users.noreply.github.com>
2019-07-13 15:57:52 -0400

    LC019 finished

--- Leetcode/LC019_Remove_Nth_Node_From_End_Of_List.java
+++ Leetcode/LC019_Remove_Nth_Node_From_End_Of_List.java
@@ -11,6 +11,24 @@ public class LC019_Remove_Nth_Node_From_End_Of_List {
 
     public ListNode removeNthFromEnd(ListNode head, int n) {
 
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        for (int i = 0; i < n; i += 1) {
+            fast = fast.next;
+        }
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
 
     }
 
