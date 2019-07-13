1460c6571a7c690ba8b0cc1b17be364aa323b173
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-07-13 15:48:33 -0400

    LC019 initiation

---
+++ Leetcode/LC019_Remove_Nth_Node_From_End_Of_List.java
@@ -0,0 +1,32 @@
+/**
+ * P019 Remove Nth Node From End of List
+ * Medium
+ *
+ *
+ * Given a linked list, remove the n-th node from the end of list and return its head.
+ * Note:
+ * Given n will always be valid.
+ */
+public class LC019_Remove_Nth_Node_From_End_Of_List {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+
+    }
+
+    public static void main(String[] args) {
+
+        ListNode q1 = ListNode.genNode(new int[]{1, 2, 3, 4, 5});
+        ListNode a1 = ListNode.genNode(new int[]{1, 2, 3, 5});
+        assert new LC019_Remove_Nth_Node_From_End_Of_List().removeNthFromEnd(q1, 2).equals(a1);
+
+        ListNode q2 = ListNode.genNode(new int[]{1});
+        assert new LC019_Remove_Nth_Node_From_End_Of_List().removeNthFromEnd(q2, 1) == null;
+
+        System.out.println("all passed");
+
+    }
+
+
+}
+
