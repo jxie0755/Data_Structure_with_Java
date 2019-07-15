f907f20c996fe5cd4111617a46d81a7ca02f799b
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-07-15 13:52:54 -0400

    Learn Java Regex

---
+++ Java_101/W3C_Java_Classes/A04b_Regex.java
@@ -0,0 +1,60 @@
+package W3C_Java_Classes;
+
+public class A04b_Regex {
+
+    /*
+     * Java 正则表达式
+     * java.util.regex 包主要包括以下三个类
+         * Pattern 类：
+             * pattern 对象是一个正则表达式的编译表示。
+             * Pattern 类没有公共构造方法。
+             * 要创建一个 Pattern 对象，你必须首先调用其公共静态编译方法，它返回一个 Pattern 对象。
+             * 该方法接受一个正则表达式作为它的第一个参数。
+         * Matcher 类：
+             * Matcher 对象是对输入字符串进行解释和匹配操作的引擎。
+             * 与Pattern 类一样，Matcher 也没有公共构造方法。
+             * 你需要调用 Pattern 对象的 matcher 方法来获得一个 Matcher 对象。
+         * PatternSyntaxException：
+            * PatternSyntaxException 是一个非强制异常类，它表示一个正则表达式模式中的语法错误。
+     */
+
+}
+
+
+class Regex_Pattern {
+
+    /*
+     * Module java.base
+     * Package java.util.regex
+     * Class Pattern
+     *
+     * A compiled representation of a regular expression.
+     * A regular expression, specified as a string, must first be compiled into an instance of this class.
+     * (类似python的概念, 可以预编译一个pattern)
+     * The resulting pattern can then be used to create a Matcher object that can match arbitrary character sequences against the regular expression.
+     */
+
+
+    /* Modifiers
+     * static int   CANON_EQ          Enables canonical equivalence.
+     * static int   CASE_INSENSITIVE  Enables case-insensitive matching.
+     * static int   COMMENTS          Permits whitespace and comments in pattern.
+     * static int   DOTALL            Enables dotall mode.
+     * static int   LITERAL	          Enables literal parsing of the pattern.
+     * static int   MULTILINE         Enables multiline mode.
+     * static int   UNICODE_CASE      Enables Unicode-aware case folding.
+     * static int   UNIX_LINES	      Enables Unix lines mode.
+     * static int   UNICODE_CHARACTER_CLASS	 Enables the Unicode version of Predefined character classes
+                                            * and POSIX character classes.
+     */
+
+
+
+    /*
+     * Pattern 基本操作
+     *
+     */
+
+
+}
+
