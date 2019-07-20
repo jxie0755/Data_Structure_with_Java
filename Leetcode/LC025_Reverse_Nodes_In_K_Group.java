b474809b6d8d9e74b0144d0f1bf32bbf317634e0
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-07-19 22:05:17 -0400

    LC025 finished

--- Leetcode/LC025_Reverse_Nodes_In_K_Group.java
+++ Leetcode/LC025_Reverse_Nodes_In_K_Group.java
@@ -13,8 +13,47 @@
  */
 public class LC025_Reverse_Nodes_In_K_Group {
 
+    private ListNode reverseNodes(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        while (head != null) {
+            ListNode rest = head.next;
+            ListNode tail = dummy.next;
+            head.next = tail;
+            dummy.next = head;
+            head = rest;
+        }
+
+        return dummy.next;
+    }
+
+
     public ListNode reverseKGroup(ListNode head, int k) {
 
+        if (k == 1 || head == null) {
+            return head;
+        }
+
+        ListNode cur = head;
+
+        // 就走一次k节
+        for (int i = 0; i < k - 1; i += 1) {
+            if (cur.next != null) {
+                cur = cur.next;
+            } else {
+                return head;
+            }
+        }
+
+        // 断开,但是不能忘记下一个节点,以备用
+        ListNode next_cur = cur.next;
+        cur.next = null;
+
+        // reverse前k个节点, 此时k-group的最后一个节点变成了反转后的头
+        ListNode new_cur = this.reverseNodes(head);
+
+        // 反转后,cur也就是tail了
+        head.next = this.reverseKGroup(next_cur, k);
+        return new_cur;
     }
 
     public static void main(String[] args) {
@@ -28,6 +67,9 @@ public class LC025_Reverse_Nodes_In_K_Group {
         assert new LC025_Reverse_Nodes_In_K_Group().reverseKGroup(Q2, 3).equals(A2): "Example 2";
 
         System.out.println("all passed");
+
+        ListNode x = ListNode.genNode(new int[]{1,2,3,4,5});
+        System.out.println(ListNode.showString(new LC025_Reverse_Nodes_In_K_Group().reverseNodes(x)));
 
     }
 
