665a4c6449813b96e6a6e992279fa8fd6f2efff5
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-07-21 00:47:38 -0400

    LC026 initiation

---
+++ Leetcode/LC026_Remove_Duplicates_From_Sorted_Array.java
@@ -0,0 +1,22 @@
+/**
+ * p026 Remove duplicates from sorted array
+ * Easy
+ *
+ * Given a sorted array, remove the duplicates in-place such that each element appear only once and return the new length.
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * 意思是不能再造一个新list,而是在原list上改动
+ */
+public class LC026_Remove_Duplicates_From_Sorted_Array {
+
+    /**
+     * # Version A
+     */
+    public int removeDuplicates(int[] nums) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
