7ad3d1bd2f7e47b848f1880d10b6b8e415d8dd90
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-07-26 13:47:45 -0400

    LC033 Initiation

--- Leetcode/LC033_Search_In_Rotated_Sorted_Array.java
+++ Leetcode/LC033_Search_In_Rotated_Sorted_Array.java
@@ -17,6 +17,49 @@ public class LC033_Search_In_Rotated_Sorted_Array {
 
     public static void main(String[] args) {
 
+        int[] Q1 = new int[]{};
+        assert new LC033_Search_In_Rotated_Sorted_Array().search(Q1, 1) == -1 : "Edge 1";
+
+        int[] Q2 = new int[]{1};
+        assert new LC033_Search_In_Rotated_Sorted_Array().search(Q2, 0) == 0 : "Edge 2";
+
+        int[] Q3 = new int[]{0};
+        assert new LC033_Search_In_Rotated_Sorted_Array().search(Q3, 0) == -1 : "Edge 3";
+
+        int[] Q4 = new int[]{4, 5, 6, 7, 0, 1, 2};
+        assert new LC033_Search_In_Rotated_Sorted_Array().search(Q4, 0) == 4 : "Example 1";
+
+        int[] Q5 = new int[]{4, 5, 6, 7, 0, 1, 2};
+        assert new LC033_Search_In_Rotated_Sorted_Array().search(Q5, 3) == -1 : "Example 2";
+
+        int[] E1 = new int[]{3, 4, 5, 6, 7, 8, 9, 10, 1, 2};
+        assert new LC033_Search_In_Rotated_Sorted_Array().search(E1, 3) == 0 : "Additional 1";
+
+        int[] E2 = new int[]{3, 4, 5, 6, 7, 8, 9, 10, 1, 2};
+        assert new LC033_Search_In_Rotated_Sorted_Array().search(E2, 4) == 1 : "Additional 2";
+
+        int[] E3 = new int[]{3, 4, 5, 6, 7, 8, 9, 10, 1, 2};
+        assert new LC033_Search_In_Rotated_Sorted_Array().search(E3, 8) == 5 : "Additional 3";
+
+        int[] E4 = new int[]{3, 4, 5, 6, 7, 8, 9, 10, 1, 2};
+        assert new LC033_Search_In_Rotated_Sorted_Array().search(E4, 10) == 7 : "Additional 4";
+
+        int[] E5 = new int[]{3, 4, 5, 6, 7, 8, 9, 10, 1, 2};
+        assert new LC033_Search_In_Rotated_Sorted_Array().search(E5, 1) == 8 : "Additional 5";
+
+        int[] E6 = new int[]{3, 4, 5, 6, 7, 8, 9, 10, 1, 2};
+        assert new LC033_Search_In_Rotated_Sorted_Array().search(E6, 2) == 9 : "Additional 6";
+
+        int[] E7 = new int[]{8, 9, 10, 1, 2, 3, 4, 5, 6, 7};
+        assert new LC033_Search_In_Rotated_Sorted_Array().search(E7, 8) == 0 : "Additional 7";
+
+        int[] E8 = new int[]{8, 9, 10, 1, 2, 3, 4, 5, 6, 7};
+        assert new LC033_Search_In_Rotated_Sorted_Array().search(E8, 1) == 3 : "Additional 8";
+
+        int[] E9 = new int[]{8, 9, 10, 1, 2, 3, 4, 5, 6, 7};
+        assert new LC033_Search_In_Rotated_Sorted_Array().search(E9, 6) == 8 : "Additional 9";
+
+        System.out.println("all passed");
     }
 
 }
