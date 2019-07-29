f0021d20b7a3fb6a182e995167ba07015485e4e7
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-07-28 23:38:12 -0400

    LC035 finished

--- Leetcode/LC035_Search_Insert_Position.java
+++ Leetcode/LC035_Search_Insert_Position.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/search-insert-position/
  * p035 Search Insert Position
@@ -9,8 +11,23 @@
  */
 public class LC035_Search_Insert_Position {
 
+    /**
+     * # Version A, sort first
+     */
     public int searchInsert(int[] nums, int target) {
 
+        int[] new_nums = Arrays.copyOf(nums, nums.length + 1);
+        new_nums[new_nums.length - 1] = target;
+        Arrays.sort(new_nums);
+
+        int index = 0;
+        while (index < new_nums.length) {
+            if (new_nums[index] == target) {
+                return index;
+            }
+            index += 1;
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
@@ -21,7 +38,7 @@ public class LC035_Search_Insert_Position {
         int[] Q = new int[]{1, 3, 5, 6};
         assert new LC035_Search_Insert_Position().searchInsert(Q, 5) == 2 : "In the list";
         assert new LC035_Search_Insert_Position().searchInsert(Q, 2) == 1 : "Not in the list";
-        assert new LC035_Search_Insert_Position().searchInsert(Q, 2) == 7 : "At the end";
+        assert new LC035_Search_Insert_Position().searchInsert(Q, 7) == 4 : "At the end";
         assert new LC035_Search_Insert_Position().searchInsert(Q, 0) == 0 : "At the beginning";
         System.out.println("all passed");
     }
