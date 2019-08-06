ab0d346a7c7e084fbf2a35306e33020183940dc7
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-08-06 09:32:37 -0400

    LC043 finished

--- Leetcode/LC043_Multiple_Strings.java
+++ Leetcode/LC043_Multiple_Strings.java
@@ -1,6 +1,3 @@
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * https://leetcode.com/problems/multiply-strings/
  * P043 Multiply Strings
@@ -17,43 +14,92 @@ import java.util.Map;
  */
 public class LC043_Multiple_Strings {
 
-    static Map<Character, Integer> hmp_s2n = new HashMap<>();
-
-    static {
-        hmp_s2n.put('0', 0);
-        hmp_s2n.put('1', 1);
-        hmp_s2n.put('2', 2);
-        hmp_s2n.put('3', 3);
-        hmp_s2n.put('4', 4);
-        hmp_s2n.put('5', 5);
-        hmp_s2n.put('6', 6);
-        hmp_s2n.put('7', 7);
-        hmp_s2n.put('8', 8);
-        hmp_s2n.put('9', 9);
-    }
-
-
     /**
      * Version C
      * HashMap method
-     * Break down to add with manual method
+     * Break down to add with manual method, with string add method
      * This can completely avoid overflow of integer/long numbers
      */
     public String multiply(String num1, String num2) {
 
-        return "1";
+        if (num1.equals("0")) {
+            return "0";
+        }
 
+        String result = "0";
+
+        int idx2 = num2.length() - 1;
+        int factor = 0;
+        while (idx2 >= 0) {
+            String to_add = "0";
+            int d2 = Character.getNumericValue(num2.charAt(idx2));
+
+            for (int i = 0; i < d2; i += 1) {
+                to_add = this.add(to_add, num1);
+            }
+
+            // to add补位0
+            to_add += new String(new char[factor]).replace("\0", "0");
+
+            // result相加本次乘法结果
+            result = this.add(result, to_add);
+
+            factor += 1;
+            idx2 -= 1;
+        }
+
+        return result;
+    }
+
+    // Helper
+    // String "add" function
+    private String add(String num1, String num2) {
+
+        StringBuilder result = new StringBuilder();
+        int add_on = 0;
+        int idx1 = num1.length() - 1;
+        int idx2 = num2.length() - 1;
+
+        while (idx1 >= 0 || idx2 >= 0) {
+
+            int d1;
+            int d2;
+
+            if (idx1 >= 0) {
+                d1 = Character.getNumericValue(num1.charAt(idx1));
+            } else {
+                d1 = 0;
+            }
+
+            if (idx2 >= 0) {
+                d2 = Character.getNumericValue(num2.charAt(idx2));
+            } else {
+                d2 = 0;
+            }
+
+            int d_result = d1 + d2 + add_on;
+            result.insert(0, d_result % 10);
+            add_on = d_result / 10;
+
+            idx1 -= 1;
+            idx2 -= 1;
+        }
+
+        // 如果最后一次还要进位
+        if (add_on != 0) {
+            result.insert(0, add_on);
+        }
+
+        return result.toString();
     }
 
     public static void main(String[] args) {
-        // assert new LC043_Multiple_Strings().multiply("0", "23").equals("0") : "Edge 1";
-        // assert new LC043_Multiple_Strings().multiply("2", "23").equals("46") : "Edge 2";
-        // assert new LC043_Multiple_Strings().multiply("2", "3").equals("6") : "Example 1";
-        // assert new LC043_Multiple_Strings().multiply("123", "457").equals("56088") : "Example 1";
-        // assert new LC043_Multiple_Strings().multiply("50", "50").equals("2500") : "Extra 1";
-
-        System.out.println(new LC043_Multiple_Strings().multiply("11111111111111", "11111111111"));
+        assert new LC043_Multiple_Strings().multiply("0", "23").equals("0") : "Edge 1";
+        assert new LC043_Multiple_Strings().multiply("2", "23").equals("46") : "Edge 2";
+        assert new LC043_Multiple_Strings().multiply("2", "3").equals("6") : "Example 1";
+        assert new LC043_Multiple_Strings().multiply("123", "456").equals("56088") : "Example 1";
+        assert new LC043_Multiple_Strings().multiply("50", "50").equals("2500") : "Extra 1";
+        System.out.println("all passed");
     }
-
 }
 
