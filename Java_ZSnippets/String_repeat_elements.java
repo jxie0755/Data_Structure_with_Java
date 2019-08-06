a6cb547e448151551ee4ad0b29d88479ebebf76d
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-08-06 09:13:11 -0400

    Learn create repeating string

---
+++ Java_ZSnippets/String_repeat_elements.java
@@ -0,0 +1,13 @@
+public class String_repeat_elements {
+
+    /*
+     * This is to learn how to create a string with repeating elements
+     * In python it is easy to create: str = 5 * "i" >>> "iiiii"
+     */
+
+    public static void main(String[] args) {
+        String S = new String(new char[5]).replace("\0", "i");
+        System.out.println(S);
+    }
+}
+
