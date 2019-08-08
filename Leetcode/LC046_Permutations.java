f3a34dcc2c9ce37d9d455850f6e9c0dc6697b647
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-08-08 14:30:01 -0400

    Leetcode P046 finished

--- Leetcode/LC046_Permutations.java
+++ Leetcode/LC046_Permutations.java
@@ -81,11 +81,11 @@ class LC046_Permutations_STD {
      */
     private List<List<Integer>> result;
 
-    public List<List<Integer>> permute(int[] num) {
+    public List<List<Integer>> permute(int[] nums) {
 
         result = new LinkedList<>();
-        if (num != null) {
-            permute(0, num);
+        if (nums != null) {
+            permute(0, nums);
         }
 
         return result;
@@ -112,5 +112,26 @@ class LC046_Permutations_STD {
         int tmp = nums[x];
         nums[x] = nums[y];
         nums[y] = tmp;
+    }
+
+
+    public static void main(String[] args) {
+        int[] q1 = {1};
+        List<Integer> a1r1 = new ArrayList<>(Arrays.asList(1));
+        List<List<Integer>> a1 = new ArrayList<>(Arrays.asList(a1r1));
+        assert new LC046_Permutations_STD().permute(q1).equals(a1) : "Edge 1";
+
+        int[] q2 = {1, 2, 3};
+        List<Integer> a2r1 = new ArrayList<>(Arrays.asList(1, 2, 3));
+        List<Integer> a2r2 = new ArrayList<>(Arrays.asList(1, 3, 2));
+        List<Integer> a2r3 = new ArrayList<>(Arrays.asList(2, 1, 3));
+        List<Integer> a2r4 = new ArrayList<>(Arrays.asList(2, 3, 1));
+        List<Integer> a2r5 = new ArrayList<>(Arrays.asList(3, 1, 2));
+        List<Integer> a2r6 = new ArrayList<>(Arrays.asList(3, 2, 1));
+        List<List<Integer>> a2 = new ArrayList<>(Arrays.asList(a2r1, a2r2, a2r3, a2r4, a2r5, a2r6));
+        // assert new LC046_Permutations_STD().permute(q2).equals(a2) : "Example 1";
+        System.out.println(new LC046_Permutations_STD().permute(q2));
+        // >>> [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 2, 1], [3, 1, 2]]  // it is not perfect
+        System.out.println("all passed");
     }
 }
