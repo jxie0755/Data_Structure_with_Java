9e75e17c10626d51e4cabe5101805f552bcac806
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-08-16 11:05:09 -0400

    LC049 finished

--- Leetcode/LC049_Group_Anagrams.java
+++ Leetcode/LC049_Group_Anagrams.java
@@ -24,54 +24,45 @@ public class LC049_Group_Anagrams {
 
         Map<List<Character>, List<String>> hmp = new HashMap<>();
         for (String word : strs) {
-            Set<Character> word_set = new HashSet<>(Arrays.asList());
+
+            List<Character> word_list = new ArrayList<>();
             for (int i = 0; i < word.length(); i += 1) {
-                word_set.add(word.charAt(i));
+                word_list.add(word.charAt(i));
             }
-            List<Character> word_set_list = new ArrayList<>(word_set);
-            Collections.sort(word_set_list);
+            Collections.sort(word_list);
+            List<Character> word_tuple = Collections.unmodifiableList(word_list);
 
-            if (!hmp.containsKey(word_set_list)) {
-                hmp.put(word_set_list, new ArrayList<>(Arrays.asList(word)));
+            if (!hmp.containsKey(word_tuple)) {
+                hmp.put(word_tuple, new ArrayList<>(Arrays.asList(word)));
             } else {
-                hmp.get(word_set_list).add(word);
+                hmp.get(word_tuple).add(word);
             }
 
         }
-        for (List<String> group : hmp.values()) {
-            result.add(group);
-        }
-        return result;
-
+        return new ArrayList<>(hmp.values());
     }
 
     public static void main(String[] args) {
 
-        // String[] q1 = {};
-        // List<String> a1l1 = new ArrayList<>(Arrays.asList());
-        // List<List<String>> a1 = new ArrayList<>(Arrays.asList(a1l1));
-        // assert new LC049_Group_Anagrams().groupAnagrams(q1).equals(a1);
-        //
-        // String[] q2 = {"a"};
-        // List<String> a2l1 = new ArrayList<>(Arrays.asList("a"));
-        // List<List<String>> a2 = new ArrayList<>(Arrays.asList(a2l1));
-        // assert new LC049_Group_Anagrams().groupAnagrams(q2).equals(a2);
-        //
+        String[] q1 = {};
+        List<String> a1l1 = new ArrayList<>(Arrays.asList());
+        List<List<String>> a1 = new ArrayList<>(Arrays.asList(a1l1));
+        assert new LC049_Group_Anagrams().groupAnagrams(q1).equals(a1);
+
+        String[] q2 = {"a"};
+        List<String> a2l1 = new ArrayList<>(Arrays.asList("a"));
+        List<List<String>> a2 = new ArrayList<>(Arrays.asList(a2l1));
+        assert new LC049_Group_Anagrams().groupAnagrams(q2).equals(a2);
+
         String[] q3 = {"eat", "tea", "tan", "ate", "nat", "bat"};
         List<String> a3l1 = new ArrayList<>(Arrays.asList("eat", "tea", "ate"));
         List<String> a3l2 = new ArrayList<>(Arrays.asList("tan", "nat"));
         List<String> a3l3 = new ArrayList<>(Arrays.asList("bat"));
-        List<List<String>> a3 = new ArrayList<>(Arrays.asList(a3l1, a3l2, a3l3));
+        List<List<String>> a3 = new ArrayList<>(Arrays.asList(a3l3, a3l2, a3l1));
         System.out.println((new LC049_Group_Anagrams().groupAnagrams(q3)));
-        // assert new LC049_Group_Anagrams().groupAnagrams(q3).equals(a3);
-        //
-        // System.out.println("all passed");
+        assert new LC049_Group_Anagrams().groupAnagrams(q3).equals(a3) : "Example 1";
 
-        String a = "wordddddddd";
-
-
-        // System.out.println(s1);
-
+        System.out.println("all passed");
     }
 }
 
