7f28085f2ac46bd82ef7aec3d4be0b7f686be76f
jxie0755 <jxie0755@users.noreply.github.com>
2019-08-28 20:25:07 -0400

    LC054 initiation

---
+++ Leetcode/LC054_Spiral_Matrix.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/spiral-matrix/
+ * P054 Spiral Matrix
+ * Medium
+ * <p>
+ * Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+ */
+public class LC054_Spiral_Matrix {
+
+    /**
+     * Version B
+     * Direct output, no buffer needed
+     */
+    public List<Integer> spiralOrder(int[][] matrix) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
