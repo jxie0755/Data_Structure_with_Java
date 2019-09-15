2e4719242296f98dc2621514cb329f57bfbf640a
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-09-14 21:14:05 -0400

    LC058 passed

---
+++ Leetcode/LC058_Length_Of_Last_Word.java
@@ -0,0 +1,42 @@
+/**
+ * https://leetcode.com/problems/length-of-last-word/
+ * p058 Length of Last word
+ * Easy
+ * <p>
+ * Given a string s consists of upper/lower-case alphabets and empty space characters " ", return the length of last word in the string.
+ * If the last word does not exist, return 0
+ * <p>
+ * Note:
+ * A word is defined as a character sequence consists of non-space characters only.
+ */
+public class LC058_Length_Of_Last_Word {
+
+    /**
+     * Version B
+     */
+    public int lengthOfLastWord(String s) {
+        int count = 0;
+        int idx = s.length() - 1;
+        while (idx >= 0) {
+            char x = s.charAt(idx);
+            if (x == ' ') {
+                idx -= 1;
+                if (count != 0) {
+                    break;
+                }
+            } else {
+                idx -= 1;
+                count += 1;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        assert new LC058_Length_Of_Last_Word().lengthOfLastWord("") == 0 : "Edge 1";
+        assert new LC058_Length_Of_Last_Word().lengthOfLastWord(" ") == 0 : "Edge 2";
+        assert new LC058_Length_Of_Last_Word().lengthOfLastWord("Hello World") == 5 : "Regular";
+        System.out.println("all passed");
+    }
+}
+
