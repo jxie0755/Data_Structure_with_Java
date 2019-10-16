5c323be3b18f745e2ab0aa3c3a92244d3446a6de
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-10-16 13:41:22 -0400

    LC062 version finished with 3 versions: 1. Combination directly, but limited by big number 2. Iteration method that requires building a array grid 3. Recursive method with memorization

--- Leetcode/LC062_Unique_Path.java
+++ Leetcode/LC062_Unique_Path.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * https://leetcode.com/problems/unique-paths/
  * P062 Unique Paths
@@ -9,7 +11,9 @@
  * <p>
  * How many possible unique paths are there?
  */
-class LC062_Unique_Path_A {
+
+
+public class LC062_Unique_Path {
 
     /**
      * Version A, best direct math calculation, FAILED
@@ -18,48 +22,64 @@ class LC062_Unique_Path_A {
      * 无法处理大数问题, 阶乘数大了之后, 溢出了int和long的范围
      * 除非使用string做计算再转换,这样太麻烦
      */
-    public int uniquePaths(int m, int n) {
-        int total = m + n - 2;
-        long result = factorial(total) / (factorial(total - (n - 1)) * factorial(n - 1));
-        return (int) result;
-    }
+    // public int uniquePaths(int m, int n) {
+    //     int total = m + n - 2;
+    //     long result = factorial(total) / (factorial(total - (n - 1)) * factorial(n - 1));
+    //     return (int) result;
+    // }
+    //
+    // /**
+    //  * Helper: factorial calculation
+    //  */
+    // private long factorial(int x) {
+    //     if (x == 0) {
+    //         return 1;
+    //     } else {
+    //         return x * factorial(x - 1);
+    //     }
+    // }
+
 
     /**
-     * Helper: factorial calculation
+     * Version B, ecursive method with hashmap memorizaton
+     * Apply in java, keep the hashmap as a class attribute
      */
-    private long factorial(int x) {
-        if (x == 0) {
+    Map<List<Integer>, Integer> HMP = new HashMap<>();
+
+    public int uniquePaths(int m, int n) {
+
+        List<Integer> pair = new ArrayList<>(Arrays.asList(m, n));
+        if (m <= 1 || n <= 1) {
             return 1;
+        } else if (HMP.containsKey(pair)) {
+            return HMP.get(pair);
         } else {
-            return x * factorial(x - 1);
+            int result = this.uniquePaths(m - 1, n) + this.uniquePaths(m, n - 1);
+            HMP.put(pair, result);
+            return result;
         }
     }
-}
 
-
-public class LC062_Unique_Path {
 
     /**
      * Version C, use grid iteration
      * Get last grid value by adding the value of the neighbors from up and left
      * This can avoid large number factorial calculation
      */
-    public int uniquePaths(int m, int n) {
-
-        int[][] grid = new int[n][m];
-        for (int i = 0; i < n; i += 1) {
-            for (int j = 0; j < m; j += 1) {
-                if (i == 0 || j == 0) {
-                    grid[i][j] = 1;
-                } else {
-                    grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
-                }
-            }
-        }
-        return grid[n - 1][m - 1];
-    }
-
-
+    // public int uniquePaths(int m, int n) {
+    //
+    //     int[][] grid = new int[n][m];
+    //     for (int i = 0; i < n; i += 1) {
+    //         for (int j = 0; j < m; j += 1) {
+    //             if (i == 0 || j == 0) {
+    //                 grid[i][j] = 1;
+    //             } else {
+    //                 grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+    //             }
+    //         }
+    //     }
+    //     return grid[n - 1][m - 1];
+    // }
     public static void main(String[] args) {
         assert new LC062_Unique_Path().uniquePaths(1, 1) == 1 : "Edge 1";
         assert new LC062_Unique_Path().uniquePaths(4, 1) == 1 : "Edge 2";
@@ -70,7 +90,6 @@ public class LC062_Unique_Path {
         assert new LC062_Unique_Path().uniquePaths(23, 12) == 193536720 : "Example 3, large number";
         System.out.println("all passed");
     }
-
 }
 
 
