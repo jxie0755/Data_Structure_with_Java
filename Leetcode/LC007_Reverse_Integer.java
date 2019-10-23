ff0a75de3ff52841270538b62c9544af0eee99c2
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-10-23 17:00:01 -0400

    Update

--- Leetcode/LC007_Reverse_Integer.java
+++ Leetcode/LC007_Reverse_Integer.java
@@ -14,35 +14,35 @@ class LC007_Reverse_Integer {
      * Version A, use string method, time O(logN), takes extra space O(LogN)
      * This is not very fast as it convert types back and forth
      */
-    public int reverse_string(int x) {  // renamed
-
-        StringBuilder result_string = new StringBuilder();
-        String current = String.valueOf(x);
-
-        Long ans;  // Handle the integer overflow by create ans number in Long first
-
-        if (x >= 0) {
-            for (int i = current.length() - 1; i >= 0; i -= 1) {
-                result_string.append(current.charAt(i));
-            }
-            ans = Long.valueOf(result_string.toString());  // Direct convert string to Long
-
-        } else {
-            for (int i = current.length() - 1; i > 0; i -= 1) {
-                result_string.append(current.charAt(i));
-            }
-            ans = Long.valueOf(result_string.toString()); // Direct convert string to Long
-        }
-
-        if (ans <= 2147483647) {  // assure no overlfow integer
-            if (x >= 0) {
-                return ans.intValue();  // convert Long value back to int for return
-            } else {
-                return -1 * ans.intValue();
-            }
-        }
-        return 0;
-    }
+    // public int reverse(int x) {  // renamed
+    //
+    //     StringBuilder result_string = new StringBuilder();
+    //     String current = String.valueOf(x);
+    //
+    //     Long ans;  // Handle the integer overflow by create ans number in Long first
+    //
+    //     if (x >= 0) {
+    //         for (int i = current.length() - 1; i >= 0; i -= 1) {
+    //             result_string.append(current.charAt(i));
+    //         }
+    //         ans = Long.valueOf(result_string.toString());  // Direct convert string to Long
+    //
+    //     } else {
+    //         for (int i = current.length() - 1; i > 0; i -= 1) {
+    //             result_string.append(current.charAt(i));
+    //         }
+    //         ans = Long.valueOf(result_string.toString()); // Direct convert string to Long
+    //     }
+    //
+    //     if (ans <= 2147483647) {  // assure no overlfow integer
+    //         if (x >= 0) {
+    //             return ans.intValue();  // convert Long value back to int for return
+    //         } else {
+    //             return -1 * ans.intValue();
+    //         }
+    //     }
+    //     return 0;
+    // }
 
 
     /**
