602e1aa9a37ea55a27cc81146f911b696102f5ea
jxie0755 <jxie0755@users.noreply.github.com>
2019-11-25 19:28:35 -0500

    LC079 initiation

---
+++ Leetcode/LC079_Word_Search.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/word-search/
+ * P079 Word Search
+ * Medium
+ * <p>
+ * Given a 2D board and a word, find if the word exists in the grid.
+ * The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+ */
+public class LC079_Word_Search {
+
+    /**
+     * Version A
+     */
+    public boolean exist(char[][] board, String word) {
+
+    }
+
+    /**
+     * A helper function to find whether a word can be found in the matrix
+     * 需要使用哈希set来记忆之前走过哪些坐标
+     */
+    private boolean finder(char[][] board, String word, int idx, List<Integer> coor, HashSet<List<Integer>> prev) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
