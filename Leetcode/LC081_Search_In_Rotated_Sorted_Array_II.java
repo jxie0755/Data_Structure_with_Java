2ea19893416b3527e4de6f5d4583e25a24f25422
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2020-01-03 22:45:13 -0500

    LC081 initiation

---
+++ Leetcode/LC081_Search_In_Rotated_Sorted_Array_II.java
@@ -0,0 +1,23 @@
+/**
+ * https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+ * P081 Search in Rotated Sorted Array II
+ * Medium
+ * <p>
+ * <p>
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+ * (i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
+ * You are given a target value to search. If found in the array return true, otherwise return false.
+ * <p>
+ * <p>
+ * Follow up:
+ * This is a follow up problem to Search in Rotated Sorted Array, where nums may contain duplicates.
+ * Would this affect the run-time complexity? How and why?
+ * <p>
+ * before: [0,1,2,3,4,5,6] might become [3,4,5,6,0,1,2]).
+ * now:    [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]
+ */
+class LC081_Search_In_Rotated_Sorted_Array_II {
+
+
+}
+
