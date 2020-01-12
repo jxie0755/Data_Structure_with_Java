5be1b3b0087a81647d7e2c97d2064694139a412e
jxie0755 <jxie0755@users.noreply.github.com>
2020-01-11 22:03:36 -0500

    Update format of test cases

--- Leetcode/LC083_Remove_Duplicates_From_Sorted_List.java
+++ Leetcode/LC083_Remove_Duplicates_From_Sorted_List.java
@@ -34,21 +34,17 @@ public class LC083_Remove_Duplicates_From_Sorted_List {
 
 
         ListNode q1 = ListNode.genNode(new int[]{});
-        ListNode a1 = ListNode.genNode(new int[]{});
-        assert testCase.deleteDuplicates(q1) == null : "Empty";
+        assert ListNode.showString(testCase.deleteDuplicates(q1)).equals("None") : "Empty";
         // 注意这里不能使用.equals因为null无法比较
 
         ListNode q2 = ListNode.genNode(new int[]{4});
-        ListNode a2 = ListNode.genNode(new int[]{4});
-        assert testCase.deleteDuplicates(q2).equals(a2) : "Single";
+        assert ListNode.showString(testCase.deleteDuplicates(q2)).equals("4") : "Single";
 
         ListNode q3 = ListNode.genNode(new int[]{1, 1, 2, 3, 3,});
-        ListNode a3 = ListNode.genNode(new int[]{1, 2, 3});
-        assert testCase.deleteDuplicates(q3).equals(a3) : "Example 1";
+        assert ListNode.showString(testCase.deleteDuplicates(q3)).equals("1->2->3");
 
         ListNode q4 = ListNode.genNode(new int[]{1, 1, 2});
-        ListNode a4 = ListNode.genNode(new int[]{1, 2});
-        assert testCase.deleteDuplicates(q4).equals(a4) : "Example 2";
+        assert ListNode.showString(testCase.deleteDuplicates(q4)).equals("1->2") : "Example 2";
 
         System.out.println("all passed");
     }
