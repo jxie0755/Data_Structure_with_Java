e39442fcaad57a94d9362017aa465c8ba9805adf
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2020-02-20 23:33:32 -0500

    Adding a Java question

---
+++ Java_Simple_Learnings/Casting_Quiz.java
@@ -0,0 +1,14 @@
+class Casting_Quiz {
+    // TODO: https://www.yinwang.org/blog-cn/2020/02/13/java-type-system
+    public static void f() {
+        String[] a = new String[2];
+        Object[] b = a;
+        a[0] = "hi";
+        // b[1] = Integer.valueOf(42);
+    }
+
+    public static void main(String[] args) {
+        f();
+    }
+}
+
