13b4ef9886baffc5da60ded445494008e3b3c025
jxie0755 <jxie0755@users.noreply.github.com>
2020-09-19 14:48:46 -0400

    Learn Sum in different data strucutures

--- Java_ZSnippets/Iterable_Sum.java
+++ Java_ZSnippets/Iterable_Sum.java
@@ -15,6 +15,13 @@ public class Iterable_Sum {
         int sum_1 = Arrays.stream(array_1).sum();
         System.out.println(sum_1);
 
+        // Array Integer[]
+        // sum()方法仅支持原始类型流
+        // 如果我们想使用包装类型Integer流, 我们必须使用mapToInt方法转换至IntStream, 再使用sum方法
+        Integer[] array_1b = new Integer[]{1, 2, 3};
+        int sum_1b = Arrays.stream(array_1b).mapToInt(Integer::intValue).sum();
+        System.out.println(sum_1b);
+
         // ArrayList (List<Integer)
         List<Integer> list_1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
         int sum_2 = list_1.stream().mapToInt(x -> x).sum();
