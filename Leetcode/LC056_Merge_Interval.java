b5c7ddb8d63e7c0e6093534c4fc29914e601e0f5
jxie0755 <jxie0755@users.noreply.github.com>
2020-11-07 19:59:13 -0500

    Code Review LC056_Merge_Interval

--- Leetcode/LC056_Merge_Interval.java
+++ Leetcode/LC056_Merge_Interval.java
@@ -29,15 +29,15 @@ class LC056_Merge_Interval {
         // sort list
         itvlist.sort((o1, o2) -> o1[0] - o2[0]);
 
-        int i = 0;
-        while (i != itvlist.size() - 1) {
-            int[] first = itvlist.get(i);
-            int[] second = itvlist.get(i + 1);
+        int idx = 0;
+        while (idx != itvlist.size() - 1) {
+            int[] first = itvlist.get(idx);
+            int[] second = itvlist.get(idx + 1);
             if (first[1] >= second[0]) {
                 first[1] = Math.max(first[1], second[1]); // necessary, because [1,4] will be sorted before [2,3]
-                itvlist.remove(i + 1);
+                itvlist.remove(idx + 1);
             } else {
-                i += 1;
+                idx += 1;
             }
         }
 
@@ -48,29 +48,35 @@ class LC056_Merge_Interval {
     public static void main(String[] args) {
         LC056_Merge_Interval testCase = new LC056_Merge_Interval();
 
-        int[][] E0 = {{0, 1}};
+        int[][] E0 = {};
         assert Arrays.deepEquals(
                 testCase.merge(E0),
+                new int[][]{}
+        ): "Edge 0";
+
+        int[][] E1 = {{0, 1}};
+        assert Arrays.deepEquals(
+                testCase.merge(E1),
                 new int[][]{{0, 1}}
-        );
+        ) : "Edge 1";
 
         int[][] S1 = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
         assert Arrays.deepEquals(
                 testCase.merge(S1),
                 new int[][]{{1, 6}, {8, 10}, {15, 18}}
-        );
+        ) : "Example 1";
 
         int[][] S1B = {{15, 18}, {1, 3}, {8, 10}, {2, 6}};  // unsorted of S1
         assert Arrays.deepEquals(
                 testCase.merge(S1B),
                 new int[][]{{1, 6}, {8, 10}, {15, 18}}
-        );
+        ) : "Example 1b unsorted";
 
         int[][] S2 = {{1, 4}, {4, 5}};
         assert Arrays.deepEquals(
                 testCase.merge(S2),
                 new int[][]{{1, 5}}
-        );
+        ) : "Example 2";
 
         System.out.println("all passed");
     }
