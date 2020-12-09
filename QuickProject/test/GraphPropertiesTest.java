18f257031c8b4f99c7ca796fe3c70ef93df946ba
jxie0755 <jxie0755@users.noreply.github.com>
2020-12-08 23:48:46 -0500

    Add more test case

--- QuickProject/test/GraphPropertiesTest.java
+++ QuickProject/test/GraphPropertiesTest.java
@@ -45,4 +45,72 @@ class GraphPropertiesTest {
 		assertEquals(3, gp.grith());
 	}
 
+	@Test
+	void testGrith_4() {
+		Graph graph = new Graph(16);
+		graph.addEdge(0, 1);
+		graph.addEdge(1, 2);
+		graph.addEdge(2, 3);
+		graph.addEdge(3, 0);
+
+		graph.addEdge(4, 5);
+		graph.addEdge(5, 6);
+		graph.addEdge(6, 7);
+		graph.addEdge(7, 8);
+		graph.addEdge(8, 4);
+
+		graph.addEdge(9, 10);
+		graph.addEdge(10, 11);
+		graph.addEdge(11, 12);
+		graph.addEdge(12, 13);
+		graph.addEdge(13, 14);
+		graph.addEdge(14, 15);
+		graph.addEdge(15, 16);
+		graph.addEdge(16, 9);
+		GraphProperties gp = new GraphProperties(graph);
+		assertEquals(4, gp.grith());
+	}
+
+	@Test
+	void testGrith_5 () {
+		Graph graph = new Graph(16);
+		graph.addEdge(0, 1);
+		graph.addEdge(1, 2);
+		graph.addEdge(2, 3);
+		graph.addEdge(3, 0);
+
+		graph.addEdge(4, 5);
+		graph.addEdge(5, 6);
+		graph.addEdge(6, 7);
+		graph.addEdge(7, 8);
+		graph.addEdge(8, 4);
+
+		graph.addEdge(4, 0);
+		graph.addEdge(2, 6);
+
+		GraphProperties gp = new GraphProperties(graph);
+		assertEquals(4, gp.grith());
+	}
+
+	@Test
+	void testGrith_6() {
+		Graph graph = new Graph(16);
+		graph.addEdge(0, 1);
+		graph.addEdge(1, 2);
+		graph.addEdge(2, 3);
+		graph.addEdge(3, 0);
+
+		graph.addEdge(4, 5);
+		graph.addEdge(5, 6);
+		graph.addEdge(6, 7);
+		graph.addEdge(7, 8);
+		graph.addEdge(8, 4);
+
+		graph.addEdge(4, 0);
+		graph.addEdge(8, 0);
+		graph.addEdge(2, 6);
+
+		GraphProperties gp = new GraphProperties(graph);
+		assertEquals(3, gp.grith());
+	}
 }
