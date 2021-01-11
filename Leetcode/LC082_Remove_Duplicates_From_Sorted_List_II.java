b40bdcfc15fab764a35222f00a1cf393ef068242
jxie0755 <jxie0755@users.noreply.github.com>
2021-01-10 22:05:18 -0500

    Code Revie LC082_Remove_Duplicates_From_Sorted_List_II

--- Leetcode/LC082_Remove_Duplicates_From_Sorted_List_II.java
+++ Leetcode/LC082_Remove_Duplicates_From_Sorted_List_II.java
@@ -8,46 +8,37 @@
 public class LC082_Remove_Duplicates_From_Sorted_List_II {
 
     /**
-     * Version B
+     * Version C
      */
     public ListNode deleteDuplicates(ListNode head) {
 
         if (head == null || head.next == null) {
             return head;
-        }
-
-        ListNode dummy = new ListNode(Integer.MAX_VALUE);
-        ListNode cur = dummy;
-        dummy.next = head;
-        ListNode tail = head.next;
-        boolean repeat = false;
-
-        while (tail != null) {
-            if (head.val != tail.val) {
-                if (!repeat) {
-                    cur.next = head;
-                    cur = cur.next;
-                } else {
-                    if (tail.next != null) {
-                        repeat = false;
-                    } else {
-                        cur.next = tail;
-                    }
-                }
+        } else if (head.next.next == null) {
+            if (head.val == head.next.val) {
+                return null;
             } else {
-                cur.next = null;
-                repeat = true;
+                return head;
             }
-            head = head.next;
-            tail = tail.next;
+        } else {
+            if (head.val != head.next.val && head.next.val != head.next.next.val) {
+                head.next.next = this.deleteDuplicates(head.next.next);
+                return head;
+            } else if (head.val != head.next.val && head.next.val == head.next.next.val) {
+                head.next = this.deleteDuplicates(head.next);
+                return head;
+            } else if (head.val == head.next.val && head.next.val != head.next.next.val) {
+                return this.deleteDuplicates(head.next.next);
+            } else  { // head.val == head.next.val && head.next.val == head.next.next.val
+                return this.deleteDuplicates(head.next);
+            }
         }
-        return dummy.next;
     }
 
     public static void main(String[] args) {
         LC082_Remove_Duplicates_From_Sorted_List_II testCase = new LC082_Remove_Duplicates_From_Sorted_List_II();
 
-        ListNode q1 = ListNode.genNode(new int[]{0});
+        ListNode q1 = ListNode.genNode(new int[]{});
         assert ListNode.showString(testCase.deleteDuplicates(q1)).equals("None") : "Edge 1";
 
         ListNode q2 = ListNode.genNode(new int[]{1, 1});
@@ -58,6 +49,18 @@ public class LC082_Remove_Duplicates_From_Sorted_List_II {
 
         ListNode q4 = ListNode.genNode(new int[]{1, 1, 1, 2, 3});
         assert ListNode.showString(testCase.deleteDuplicates(q4)).equals("2->3") : "Example 2";
+
+        ListNode q5 = ListNode.genNode(new int[]{1, 2, 2});
+        assert ListNode.showString(testCase.deleteDuplicates(q5)).equals("1") : "Additional 1";
+
+        ListNode q6 = ListNode.genNode(new int[]{1, 2, 2, 3});
+        assert ListNode.showString(testCase.deleteDuplicates(q6)).equals("1->3") : "Additional 2";
+
+        ListNode q7 = ListNode.genNode(new int[]{1, 2, 2, 2});
+        assert ListNode.showString(testCase.deleteDuplicates(q7)).equals("1") : "Additional 3";
+
+        ListNode q8 = ListNode.genNode(new int[]{1, 1, 1, 1});
+        assert ListNode.showString(testCase.deleteDuplicates(q8)).equals("None") : "Additional 3";
 
         System.out.println("all passed");
     }
