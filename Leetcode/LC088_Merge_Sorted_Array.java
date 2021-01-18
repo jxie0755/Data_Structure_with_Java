ea5fb4e12bac4a57ed2a42ed40e41586df17dfc2
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-01-18 15:35:14 -0500

    Code Review LC088_Merge_Sorted_Array

---
+++ Leetcode/LC088_Merge_Sorted_Array.java
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode.com/problems/merge-sorted-array/
+ * p088 Merge Sorted Array
+ * Easy
+ * <p>
+ * <p>
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+ * <p>
+ * Note:
+ * The number of elements initialized in nums1 and nums2 are m and n respectively.
+ * You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+ * <p>
+ * Key is to modified nums1, not returning a new list
+ */
+class LC088_Merge_Sorted_Array {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
