4c62058787c0050741c230129f631b6470b6d137
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-01-20 13:55:15 -0500

    Code Review LC088_Merge_Sorted_Array

--- Leetcode/LC088_Merge_Sorted_Array.java
+++ Leetcode/LC088_Merge_Sorted_Array.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/merge-sorted-array/
  * p088 Merge Sorted Array
@@ -22,7 +24,59 @@ class LC088_Merge_Sorted_Array {
     }
 
     public static void main(String[] args) {
+        LC088_Merge_Sorted_Array testCase = new LC088_Merge_Sorted_Array();
 
+        int[] q1a = new int[]{1};
+        int[] q1b = new int[]{};
+        testCase.merge(q1a, 1, q1b, 0);
+        assert Arrays.equals(q1a, new int[]{1}) : "T1";
+
+        int[] q2a = new int[]{0};
+        int[] q2b = new int[]{1};
+        testCase.merge(q2a, 0, q2b, 1);
+        assert Arrays.equals(q2a, new int[]{1}) : "T2";
+
+        int[] q3a = new int[]{1, 2, 3, 0, 0, 0};
+        int[] a3b = new int[]{2, 5, 6};
+        testCase.merge(q3a, 3, a3b, 3);
+        assert Arrays.equals(q3a, new int[]{1, 2, 2, 3, 5, 6}) : "T3";
+
+        int[] q4a = new int[]{1, 5, 7, 0, 0, 0};
+        int[] q4b = new int[]{2, 4, 10};
+        testCase.merge(q4a, 3, q4b, 3);
+        assert Arrays.equals(q4a, new int[]{1, 2, 4, 5, 7, 10}) : "T4";
+
+        int[] q5a = new int[]{8, 8, 8, 0, 0, 0};
+        int[] q5b = new int[]{1, 2, 3};
+        testCase.merge(q5a, 3, q5b, 3);
+        assert Arrays.equals(q5a, new int[]{1, 2, 3, 8, 8, 8}) : "T5";
+
+        int[] q6a = new int[]{1, 0, 0, 0};
+        int[] q6b = new int[]{5, 5, 5};
+        testCase.merge(q6a, 1, q6b, 3);
+        assert Arrays.equals(q6a, new int[]{1, 5, 5, 5}) : "T6";
+
+        int[] q7a = new int[]{1, 2, 4, 5, 6, 0};
+        int[] q7b = new int[]{3};
+        testCase.merge(q7a, 5, q7b, 1);
+        assert Arrays.equals(q7a, new int[]{1, 2, 3, 4, 5, 6}) : "T7";
+
+        int[] q8a = new int[]{4, 0, 0, 0, 0, 0};
+        int[] q8b = new int[]{1, 2, 3, 5, 6};
+        testCase.merge(q8a, 1, q8b, 5);
+        assert Arrays.equals(q8a, new int[]{1, 2, 3, 4, 5, 6}) : "T8";
+
+        int[] q9a = new int[]{4, 0, 0, 0, 0, 0, 0};
+        int[] q9b = new int[]{1, 2, 3, 5, 6};
+        testCase.merge(q9a, 1, q9b, 5);
+        assert Arrays.equals(q9a, new int[]{1, 2, 3, 4, 5, 6, 0}) : "T9";
+
+        int[] q10a = new int[]{-1, 0, 0, 3, 3, 3, 0, 0, 0};
+        int[] q10b = new int[]{1, 2, 2};
+        testCase.merge(q10a, 6, q10b, 3);
+        assert Arrays.equals(q10a, new int[]{1, 0, 0, 1, 2, 2, 3, 3, 3}) : "T10";
+
+        System.out.println("All passed");
     }
 }
 
