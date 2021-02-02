d6b4a37b8434106a4a5df54205bb3befc8596a5d
jxie0755 <jxie0755@users.noreply.github.com>
2021-02-02 15:47:48 -0500

    Code Review LC098_Validate_Binary_Search_Tree

---
+++ Leetcode/LC098_Validate_Binary_Search_Tree.java
@@ -0,0 +1,23 @@
+/**
+ * https://leetcode.com/problems/validate-binary-search-tree/
+ * P098 Validate Binary Search Tree
+ * Medium
+ * <p>
+ * <p>
+ * Given a binary tree, determine if it is a valid binary search tree (BST).
+ * <p>
+ * Assume a BST is defined as follows:
+ * The left subtree of a node contains only nodes with keys less than the node's key.
+ * The right subtree of a node contains only nodes with keys greater than the node's key.
+ * <p>
+ * Both the left and right subtrees must also be binary search trees.
+ */
+public class LC098_Validate_Binary_Search_Tree {
+    /**
+     * Version STD_B Variation of Morris Traversal method
+     */
+    public boolean isValidBST(TreeNode root) {
+
+    }
+}
+
