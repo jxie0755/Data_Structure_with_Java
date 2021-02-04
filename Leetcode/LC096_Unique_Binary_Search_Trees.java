ac259f3f417944a2abfe14e48f856fb7cae8553d
jxie0755 <jxie0755@users.noreply.github.com>
2021-02-03 21:49:49 -0500

    Code Review LC096_Unique_Binary_Search_Trees

---
+++ Leetcode/LC096_Unique_Binary_Search_Trees.java
@@ -0,0 +1,21 @@
+/**
+ * https://leetcode.com/problems/unique-binary-search-trees/
+ * P096 Unique Binary Search Trees
+ * Medium
+ * <p>
+ * Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
+ */
+public class LC096_Unique_Binary_Search_Trees {
+    /**
+     * Version A3, Memorization method, use inner class setup
+     */
+    public int numTrees(int n) {
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
