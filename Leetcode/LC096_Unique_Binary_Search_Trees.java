71522d20af8e02b5d0c1a8316640f1f0944612b5
jxie0755 <jxie0755@users.noreply.github.com>
2021-02-03 22:10:37 -0500

    Code Review LC096_Unique_Binary_Search_Trees

--- Leetcode/LC096_Unique_Binary_Search_Trees.java
+++ Leetcode/LC096_Unique_Binary_Search_Trees.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/unique-binary-search-trees/
  * P096 Unique Binary Search Trees
@@ -10,12 +13,45 @@ public class LC096_Unique_Binary_Search_Trees {
      * Version A3, Memorization method, use inner class setup
      */
     public int numTrees(int n) {
-        return 0;
+
+        Map<Integer, Integer> hmp = new HashMap<>(Map.ofEntries(
+                Map.entry(0, 1),
+                Map.entry(1, 1)
+        ));
+
+
+        /**
+         * Inner class helper for memorization
+         */
+        class Helper {
+            private int numTreesHelper(int n) {
+                if (hmp.containsKey(n)) {
+                    return hmp.get(n);
+                } else {
+                    int result = 0;
+                    for (int rt = 1; rt <= n; rt += 1) {
+                        int prev = rt - 1;
+                        int after = n - rt;
+                        result += this.numTreesHelper(prev) * this.numTreesHelper(after);
+                    }
+                    hmp.put(n, result);
+                    return result;
+                }
+            }
+        }
+
+        Helper h = new Helper();
+        return h.numTreesHelper(n);
     }
 
 
     public static void main(String[] args) {
+        LC096_Unique_Binary_Search_Trees testCase = new LC096_Unique_Binary_Search_Trees();
 
+        assert testCase.numTrees(3) == 5 : "Example 1";
+        assert testCase.numTrees(19) == 1767263190 : "Long 1";
+
+        System.out.println("All passed");
     }
 }
 
