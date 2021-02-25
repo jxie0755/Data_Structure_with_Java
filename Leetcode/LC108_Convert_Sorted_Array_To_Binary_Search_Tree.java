04890239be22ea6fa55b42218e24351a3255af06
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-02-24 21:27:15 -0500

    Code Review LC108_Convert_Sorted_Array_To_Binary_Search_Tree

--- Leetcode/LC108_Convert_Sorted_Array_To_Binary_Search_Tree.java
+++ Leetcode/LC108_Convert_Sorted_Array_To_Binary_Search_Tree.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
  * P108 Convert Sorted Array to Binary Search Tree
@@ -13,22 +15,31 @@ class LC108_Convert_Sorted_Array_To_Binary_Search_Tree {
      * Version A
      */
     public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
 
+        int mid_idx = nums.length / 2;
+        int mid_val = nums[mid_idx];
+        TreeNode T = new TreeNode(mid_val);
+        T.left = this.sortedArrayToBST(Arrays.copyOfRange(nums, 0, mid_idx));
+        T.right = this.sortedArrayToBST(Arrays.copyOfRange(nums, mid_idx + 1, nums.length));
+        return T;
     }
 
     public static void main(String[] args) {
         LC108_Convert_Sorted_Array_To_Binary_Search_Tree testCase = new LC108_Convert_Sorted_Array_To_Binary_Search_Tree();
 
         int[] A0 = new int[]{};
-        assert testCase.sortedArrayToBST(A0).equals(null) : "Edge 0";
+        assert testCase.sortedArrayToBST(A0) == null : "Edge 0";
 
-        int[] A1 = new int[]{};
+        int[] A1 = new int[]{1};
         assert testCase.sortedArrayToBST(A1).equals(TreeNode.genTree(new Object[]{
                 1
         })) : "Edge 1";
 
         int[] A2 = new int[]{-10, -3, 0, 5, 9};
-        assert testCase.sortedArrayToBST(A1).equals(TreeNode.genTree(new Object[]{
+        assert testCase.sortedArrayToBST(A2).equals(TreeNode.genTree(new Object[]{
                 0,
                 -3, 9,
                 -10, null, 5, null
