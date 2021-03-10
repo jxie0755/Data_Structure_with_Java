19d7d2c2c2deaf6c845ee08af3743f6a84b6dcc6
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-03-10 14:32:19 -0500

    Create Node Class in the head

--- Leetcode/LC116_Populating_Next_Right_Pointers_In_Each_Node.java
+++ Leetcode/LC116_Populating_Next_Right_Pointers_In_Each_Node.java
@@ -18,42 +18,6 @@ import java.util.List;
  */
 
 
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {
-    }
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-
-    public static boolean isEqual(Node self, Node other) {
-        if (self != null && other != null) {
-            return self.val == other.val &&
-                    Node.isEqual(self.left, other.left) &&
-                    Node.isEqual(self.right, other.right) &&
-                    Node.isEqual(self.next, other.next);
-        } else if (self == null && other == null) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
-
-
 class LC116_Populating_Next_Right_Pointers_In_Each_Node {
     /**
      * Version A
---
+++ Leetcode/LC117_Populating_Next_Right_Pointers_In_Each_Node_II.java
@@ -0,0 +1,17 @@
+/**
+ * https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
+ * LC117 Populating Next Right Pointers in Each Node II
+ * Medium
+ * <p>
+ * Different from LC106: You are given a non-perfect binary tree.
+ * <p>
+ * Note:
+ * Populate each next pointer to point to its next right node.
+ * If there is no next right node, the next pointer should be set to NULL.
+ * Initially, all next pointers are set to NULL.
+ */
+
+class LC117_Populating_Next_Right_Pointers_In_Each_Node_II {
+
+}
+
