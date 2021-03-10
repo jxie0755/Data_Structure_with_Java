81d6ec538fdc3256b51c202cf2646b8180d0c9e5
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-03-10 14:39:45 -0500

    Code Review LC117_Populating_Next_Right_Pointers_In_Each_Node_II

--- Leetcode/LC117_Populating_Next_Right_Pointers_In_Each_Node_II.java
+++ Leetcode/LC117_Populating_Next_Right_Pointers_In_Each_Node_II.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
  * LC117 Populating Next Right Pointers in Each Node II
@@ -12,6 +16,70 @@
  */
 
 class LC117_Populating_Next_Right_Pointers_In_Each_Node_II {
+    /**
+     * Version A
+     * Modified from levelOrderTraversal
+     */
+    public Node connect(Node root) {
+        if (root != null) {
+            List<Node> current = new ArrayList<>(Arrays.asList(root));
 
+            while (!current.isEmpty()) {
+
+                for (int lidx = 0; lidx < current.size() - 1; lidx += 1) {
+                    current.get(lidx).next = current.get(lidx + 1);
+                }
+
+
+                List<Node> next_layer = new ArrayList<>(Arrays.asList());
+
+                for (Node node : current) {
+                    if (node.left != null) {
+                        next_layer.add(node.left);
+                    }
+                    if (node.right != null) {
+                        next_layer.add(node.right);
+                    }
+                }
+                current = next_layer;
+            }
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        LC117_Populating_Next_Right_Pointers_In_Each_Node_II testCase = new LC117_Populating_Next_Right_Pointers_In_Each_Node_II();
+
+
+        Node A = new Node(1,
+                new Node(2,
+                        new Node(4, null, null, null),
+                        new Node(5, null, null, null),
+                        null),
+                new Node(3,
+                        null,
+                        new Node(7, null, null, null),
+                        null),
+                null);
+
+        Node B = new Node(1,
+                new Node(2,
+                        new Node(4, null, null, null),
+                        new Node(5, null, null, null),
+                        null),
+                new Node(3,
+                        null,
+                        new Node(7, null, null, null),
+                        null),
+                null);
+        B.left.next = B.right;
+        B.left.left.next = B.left.right;
+        B.left.right.next = B.right.right;
+
+        assert Node.isEqual(testCase.connect(A), B) : "Example 1";
+
+        System.out.println("All passed");
+    }
 }
 
