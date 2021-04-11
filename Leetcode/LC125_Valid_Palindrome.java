a3143b615c160eec29d9067873cd5694d29c1ca8
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-04-11 14:24:40 -0400

    Code Review LC125_Valid_Palindrome

---
+++ Leetcode/LC125_Valid_Palindrome.java
@@ -0,0 +1,23 @@
+/**
+ * https: // leetcode.com / problems / valid - palindrome /
+ * LC125 Valid Palindrome
+ * Easy
+ * <p>
+ * Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+ * <p>
+ * Note:
+ * Have you consider that the string might be empty? This is a good question to ask during an interview.
+ * For the purpose of this problem, we define empty string as valid palindrome.
+ */
+class LC125_Valid_Palindrome {
+
+    public boolean isPalindrome(String s) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
