33c46d8f37226c4870959b37e475a8ea2994ec49
jxie0755 <jxie0755@users.noreply.github.com>
2021-06-19 14:23:28 -0700

    LC032 initiation

---
+++ DataStructures/LC032_Longest_Valid_Parentheses.java
@@ -0,0 +1,19 @@
+/**
+ * https://leetcode.com/problems/longest-valid-parentheses/
+ * LC020 Longest Valid Parentheses
+ * Hard
+ *
+ * Given a string containing just the characters "(" and ")", find the length of the longest valid (well-formed) parentheses substring.
+ */
+
+public class LC032_Longest_Valid_Parentheses {
+
+    public int longestValidParentheses(String s) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
