93dff3a3781ea377d8775c0a36f645d1d040f1c8
jxie0755 <jxie0755@users.noreply.github.com>
2021-06-23 21:44:50 -0700

    LC138 code review

--- Leetcode/LC138_Copy_List_With_Random_Pointer.java
+++ Leetcode/LC138_Copy_List_With_Random_Pointer.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/copy-list-with-random-pointer/
  * LC139 Copy List with Random Pointer
@@ -11,26 +14,69 @@
  * You must return the copy of the given head as a reference to the cloned list.
  */
 
-/*
-// Definition for a Node.
-class Node {
-    int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
-    }
-}
-*/
-
 
 public class LC138_Copy_List_With_Random_Pointer {
 
+    /**
+     * 建立一个内部类作为测试使用
+     */
+    static class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
     public Node copyRandomList(Node head) {
 
+        Map<Node, Node> check_list = new HashMap<>();
+
+        /**
+         * Inner class helper for memorization
+         */
+        class Helper {
+            private Node help(Node head) {
+                if (head != null) {
+                    if (!check_list.containsKey(head)) {
+                        Node new_head = new Node(head.val);
+                        check_list.put(head, new_head);
+                        new_head.next = this.help(head.next);
+                        new_head.random = this.help(head.random);
+                        return new_head;
+                    } else {
+                        return check_list.get(head);
+                    }
+                } else {
+                    return head;
+                }
+            }
+        }
+        Helper h = new Helper();
+        return h.help(head);
+    }
+
+    public static void main(String[] args) {
+        Node B = new Node(2);
+        B.next = null;
+        B.random = B;
+        Node A = new Node(1);
+        A.next = B;
+        A.random = B;
+
+        LC138_Copy_List_With_Random_Pointer testCase = new LC138_Copy_List_With_Random_Pointer();
+        Node AA = testCase.copyRandomList(A);
+
+        assert AA.val == 1 : "Val";
+        assert AA.next.val == 2 : "next Val";
+        assert AA.random.val == 2 : "random val";
+        assert AA.next.random.val == 2 : "cycling";
+        assert AA.next.random == AA.next : "True self poiting";
+        System.out.println("All passed");
     }
 
 
