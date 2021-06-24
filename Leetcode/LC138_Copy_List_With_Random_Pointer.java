7848501c7e78e686330973673c01263a91e584e6
jxie0755 <jxie0755@users.noreply.github.com>
2021-06-23 21:25:09 -0700

    LC138 code review

---
+++ Leetcode/LC138_Copy_List_With_Random_Pointer.java
@@ -0,0 +1,38 @@
+/**
+ * https://leetcode.com/problems/copy-list-with-random-pointer/
+ * LC139 Copy List with Random Pointer
+ * Medium
+ *
+ * A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+ *
+ * Return a deep copy of the list.
+ *
+ * Note:
+ * You must return the copy of the given head as a reference to the cloned list.
+ */
+
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+
+public class LC138_Copy_List_With_Random_Pointer {
+
+    public Node copyRandomList(Node head) {
+
+    }
+
+
+}
+
