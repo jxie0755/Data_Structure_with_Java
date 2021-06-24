3c8abbbf2960326ea50aebe7eaadf80a66ce401c
jxie0755 <jxie0755@users.noreply.github.com>
2021-06-23 21:52:12 -0700

    Remove public Node class and use inner class in LC116 and LC117

--- Leetcode/LC116_Populating_Next_Right_Pointers_In_Each_Node.java
+++ Leetcode/LC116_Populating_Next_Right_Pointers_In_Each_Node.java
@@ -19,6 +19,45 @@ import java.util.List;
 
 
 class LC116_Populating_Next_Right_Pointers_In_Each_Node {
+
+    /**
+     * 建立一个内部Node类作为测试用
+     */
+    static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+
+        public static boolean isEqual(Node self, Node other) {
+            if (self != null && other != null) {
+                return self.val == other.val &&
+                        Node.isEqual(self.left, other.left) &&
+                        Node.isEqual(self.right, other.right) &&
+                        Node.isEqual(self.next, other.next);
+            } else if (self == null && other == null) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+
     /**
      * Version A
      * Modified from levelOrderTraversal
